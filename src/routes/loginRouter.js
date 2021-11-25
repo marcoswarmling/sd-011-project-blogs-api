14d085e9d1e4a1bfea6e3264e16d5000c9cf210e
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-25 19:14:33 -0300

    feat: add login validation to route

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const loginController = require('../controllers/loginController');
 
+const loginValidator = require('../middlewares/validations/login/loginValidator');
+
 const loginRouter = express.Router();
 
-loginRouter.post('/', loginController.signin);
+loginRouter.post('/', loginValidator, loginController.signin);
 
 module.exports = loginRouter;
