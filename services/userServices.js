e6e2250dd0ebe140dd6b8a2923c8ab3f9988298f
LeoFuna <leo.funa@gmail.com>
2021-11-25 15:44:54 -0300

    feat: validaçao grosseira do displayName

--- services/userServices.js
+++ services/userServices.js
@@ -10,6 +10,11 @@ const getAll = async () => {
 };
 
 const createUser = async (displayName, email, password, image) => {
+  console.log(displayName.length);
+  console.log(typeof displayName);
+  if (displayName.length < 8 || typeof displayName !== 'string') {
+    return { message: 'O nome deve ter no mínimo 8 caracteres...' };
+  }
   try {
     const createResponse = await User.create({ displayName, email, password, image });
     return createResponse;
