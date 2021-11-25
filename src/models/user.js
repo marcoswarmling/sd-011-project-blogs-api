bd162a88e59551f77abcb53dbb987ed6fe851592
Felipe Buso <felipe_buso@hotmail.com>
2021-11-25 15:11:31 -0300

    feat: create database

---
+++ src/models/user.js
@@ -0,0 +1,16 @@
+const User = (sequelize, DataTypes) => {
+  const user = sequelize.define('User', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'User',
+  });
+  return user;
+};
+
+module.exports = User;
