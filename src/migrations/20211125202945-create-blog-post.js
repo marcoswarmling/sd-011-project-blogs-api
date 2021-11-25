4a2ba9e4776dbc34832b11318321a31edae10fa0
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-11-25 17:39:51 -0300

    build: create Users and BlogPosts tables

---
+++ src/migrations/20211125202945-create-blog-post.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        field: 'published',
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        field: 'updated',
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('BlogPosts');
+  }
+};
---
+++ src/models/blogpost.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.Employee,
+      { foreignKey: 'userId', as: 'users' });
+  };
+
+  return BlogPost;
+};
