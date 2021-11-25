04781ed0c68c436295087c6c143f0b1ed6449410
Renan <renan.vamo@gmail.com>
2021-11-25 20:51:38 -0300

    feat: cria relaçao da tabela de ligacao com as outras tabelas

--- migrations/20211125233336-posts-categories.js
+++ migrations/20211125233336-posts-categories.js
@@ -4,14 +4,26 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('PostsCategories', {
       postId: {
+        type: Sequelize.INTEGER,
         allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'post_id',
+        references: {
+          model: 'Post',
+          key: 'id',
+        },
       },
       categoryId: {
+        type: Sequelize.INTEGER,
         allowNull: false,
-        type: Sequelize.STRING
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'category_id',
+        references: {
+          model: 'Category',
+          key: 'id',
+        },
       },
     });
   },
