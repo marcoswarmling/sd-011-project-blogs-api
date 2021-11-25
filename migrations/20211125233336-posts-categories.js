040b1cd9abcbff6345f3b89801d011d850fa52f2
Renan <renan.vamo@gmail.com>
2021-11-25 20:42:06 -0300

    feat: adiciona as migraçoes de todos os arquivos

---
+++ migrations/20211125233336-posts-categories.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostsCategories', {
+      postId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      categoryId: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostsCategories');
+  }
+};
