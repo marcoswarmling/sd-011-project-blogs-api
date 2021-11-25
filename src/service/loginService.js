137664cabc583b9a1402d9b6baed987c42a66377
Filizzola <juliofilizzola@hotmail.com>
2021-11-25 13:18:43 -0300

    FIX: espalo na loginUsersVerify

--- src/service/loginService.js
+++ src/service/loginService.js
@@ -5,7 +5,6 @@ const { User } = require('../../models');
 
 const loginUserVerify = async ({ email, password }) => {
   const result = await User.findOne({ where: { email, password } });
-  
   return result;
 };
 
