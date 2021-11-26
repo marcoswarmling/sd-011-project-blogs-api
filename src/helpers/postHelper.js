2ea876917af59586e9261947e8423b900b4b3fa5
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-26 19:07:26 -0300

    feat: add update post validation helper function

--- src/helpers/postHelper.js
+++ src/helpers/postHelper.js
@@ -2,12 +2,19 @@ const errors = require('../schemas/errorsSchema');
 
 const attributes = {
   new: ['title', 'categoryIds', 'content'],
+  update: ['title', 'content'],
 };
 
 const validateNewPostAttributes = (post) => {
-  attributes.new.forEach((field) => {
-    if (!post[field]) throw errors.requiredField(field);
+  attributes.new.forEach((attribute) => {
+    if (!post[attribute]) throw errors.requiredField(attribute);
   });
 };
 
-module.exports = { validateNewPostAttributes };
+const validateUpdatePostAttributes = (post) => {
+  attributes.update.forEach((attribute) => {
+    if (!post[attribute]) throw errors.requiredField(attribute);
+  });
+};
+
+module.exports = { validateNewPostAttributes, validateUpdatePostAttributes };
