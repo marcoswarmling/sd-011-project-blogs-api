d3b09a634eb77413fc975fc2eb9ae36b10c12efa
Amandha W. Barok <amandha.barok@gmail.com>
2021-11-26 14:38:26 -0800

    Finishing the first requiriment

--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,7 @@ const createUser = async(req, res) => {
   if(result.message) {
     return res.status(result.status).json({message: result.message});
   }
-  return res.status(201).json(result);
+  return res.status(201).json({token: result.token });
 };
 
 
--- services/user.js
+++ services/user.js
@@ -1,16 +1,51 @@
 const { User } = require('../models');
 
+const isValidEmail = (email) => {
+  const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+  return emailRegex.test(email);
+};
+
+const generateToken = (user) => {
+  return 'abcdadfskfsak1212';
+}
+
 const createUser = async(data) => {
   try {
     const { displayName, email, password, image } = data;
 
     if(displayName.length < 8) {
-      console.log('ENTROU AQUIIII')
       return { message: '"displayName" length must be at least 8 characters long', status: 400 };
     }
+
+    if(!email) {
+      return { message: '"email" is required', status: 400 }
+    }
+
+
+    if(!isValidEmail(email)) {
+      return { message: '"email" must be a valid email', status: 400 }
+    }
+
+    const userAlreadyExits = await User.findOne({ where: {
+      email: email
+    }});
+
+    console.log(userAlreadyExits)
+    if(userAlreadyExits !== null) {
+      return { message: 'User already registered', status: 409 };
+    }
+
+    if(!password) {
+      return { message: '"password" is required', status: 400 }
+    }
+
+    if(password.length < 6) {
+      return { message: '"password" length must be 6 characters long', status: 400 }
+    }
+
     const newUser = await User.create({displayName, email, password, image });
 
-    return newUser;
+    return {token: generateToken(newUser)};
   } catch (error) {
     console.log(error.message);
 
