d3fdb0554f66e38bd401f21c3d98a4b7148e8ce6
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-25 22:09:39 -0300

    adiciona middlewares em app

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userRoutes = require('../routes/userRoutes');
 const userMiddleware = require('../middlewares/userMeddlewares');
+const userMiddlewaresComplement = require('../middlewares/userMiddlewaresComplement');
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use(userRoutes);
 app.use(userMiddleware);
+app.use(userMiddlewaresComplement);
 
 module.exports = app;
