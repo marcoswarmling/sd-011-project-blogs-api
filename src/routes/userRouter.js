fb50be703ce26228f30cc5f1432f1e3b075f6024
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-25 22:54:17 -0300

    feat: add get user by id route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -9,5 +9,6 @@ const userRouter = express.Router();
 
 userRouter.post('/', userValidator, userController.create);
 userRouter.get('/', authValidator, userController.getAll);
+userRouter.get('/:id', authValidator, userController.getById);
 
 module.exports = userRouter;
