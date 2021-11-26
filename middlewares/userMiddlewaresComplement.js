7899f29ee6881d02a4e90aeefb1ca4c0f6e516d5
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-25 22:53:47 -0300

    correção escrita de erros em middlewares

--- middlewares/userMiddlewaresComplement.js
+++ middlewares/userMiddlewaresComplement.js
@@ -1,7 +1,7 @@
 module.exports = (err, _req, res, _next) => {
   if (err.code === 'invalidName') {
     return res.status(400).json({ 
-      message: 'displayName length must be at least 8 characters long',
+      message: '"displayName" length must be at least 8 characters long',
     });
   }
   return res.status(500).json({ 
