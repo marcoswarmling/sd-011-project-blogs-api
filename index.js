aeda5b798f1d76cbc0bb9e6f9441647d65736d4a
Gabriel Pimentel Fernandes <gabrielfernandessilva.ba@gmail.com>
2021-11-26 08:12:55 -0300

    Reevindo todos os commits pro evaluator funcionar

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use('/categories', categoriesRouter);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
-// não remova esse endpoint, e para o avaliador funcionar
+// não remova esse endpoint, e para o avaliador funcionar -
 app.get('/', (request, response) => {
   response.send();
 });
