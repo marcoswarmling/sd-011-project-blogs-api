5cff5cbbe5a7b235dd096fcd46a48ba1a73d6181
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-26 18:30:34 -0300

    adiciona categoryServise

---
+++ services/categoryService.js
@@ -0,0 +1,10 @@
+const { User } = require('../models');
+const { isValidUser } = require('../utils/validations');
+
+const categoryRegister = async (name) => {
+
+};
+
+module.exports = {
+  categoryRegister,
+};
