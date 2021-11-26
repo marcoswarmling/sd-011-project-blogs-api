c499c5de6610cb92823cd7dd721e21b1ebd42191
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-26 16:46:03 -0300

    adiciona getAllUsers em rota get

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controller = require('../controllers/userController');
 
 router.post('/user', controller.userRegister);
+router.get('/user', controller.getAllUsers);
 
 module.exports = router;
