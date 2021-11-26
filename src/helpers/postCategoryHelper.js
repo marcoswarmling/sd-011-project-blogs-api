ebc642c26def472edccfac3adfe3ce1b300a6ba0
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-26 16:48:15 -0300

    feat: create post category helper

---
+++ src/helpers/postCategoryHelper.js
@@ -0,0 +1,15 @@
+const postCategoryService = require('../services/postCategoryService');
+
+module.exports = {
+  createPostsCategories: async (postId, categoryIds) => {
+    const createPostCategoryTasks = [];
+  
+    for (let index = 0; index < categoryIds.length; index += 1) {
+      const categoryId = categoryIds[index];
+  
+      createPostCategoryTasks.push(postCategoryService.create({ postId, categoryId }));
+    }
+  
+    await Promise.all(createPostCategoryTasks);
+  },
+};
