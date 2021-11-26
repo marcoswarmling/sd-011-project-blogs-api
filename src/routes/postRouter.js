1879039019dc21c49904f17e90b94d111a1819cf
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-26 19:34:55 -0300

    feat: add post ownership middleware to update route

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -5,12 +5,13 @@ const postController = require('../controllers/postController');
 const authValidator = require('../middlewares/validations/auth/authValidator');
 const newPostValidator = require('../middlewares/validations/post/newPostValidator');
 const updatePostValidator = require('../middlewares/validations/post/updatePostValidator');
+const postOwnership = require('../middlewares/validations/post/postOwnership');
 
 const postRouter = express.Router();
 
 postRouter.post('/', authValidator, newPostValidator, postController.create);
 postRouter.get('/:id', authValidator, postController.getById);
 postRouter.get('/', authValidator, postController.getAll);
-postRouter.put('/:id', authValidator, updatePostValidator, postController.update);
+postRouter.put('/:id', authValidator, updatePostValidator, postOwnership, postController.update);
 
 module.exports = postRouter;
