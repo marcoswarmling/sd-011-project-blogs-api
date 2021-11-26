aa7014110e6dace35de069ec2b6fe64c6eac2012
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-26 17:51:27 -0300

    adiciona getUserById em routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ const auth = require('../middlewares/authMiddleware');
 
 router.post('/user', controller.userRegister);
 router.get('/user', auth, controller.getAllUsers);
+router.get('/user/:id', auth, controller.getUserById);
 
 module.exports = router;
