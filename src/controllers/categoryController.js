f48f681c044c8a56561c302c413250324dbe8bfe
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-25 23:07:23 -0300

    feat: add create category function to controller

---
+++ src/controllers/categoryController.js
@@ -0,0 +1,16 @@
+const statusCodes = require('../schemas/statusCodesSchema');
+const categoryService = require('../services/categoryService');
+
+module.exports = {
+  create: async (request, response, next) => {
+    const { name } = request.body;
+
+    try {
+      const category = await categoryService.create(name);
+
+      return response.status(statusCodes.created).json(category);
+    } catch (error) {
+      return next(error);
+    }
+  },
+};
