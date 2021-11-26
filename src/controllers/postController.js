e46c669c4077954854f50612000ebdf9500b1d8e
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-26 15:25:37 -0300

    fix: add user id to post creation

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -3,10 +3,11 @@ const postService = require('../services/postService');
 
 module.exports = {
   create: async (request, response, next) => {
-    const { title, content, categoryIds } = request.body;
+    const { user, body } = request;
+    const { title, content, categoryIds } = body;
 
     try {
-      const post = await postService.create({ title, content, categoryIds });
+      const post = await postService.create({ title, content, categoryIds, userId: user.id });
 
       return response.status(statusCodes.created).json(post);
     } catch (error) {
