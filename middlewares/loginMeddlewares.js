283d4f9361a927106c7b984595530421b2966ba0
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-26 12:12:55 -0300

    adiciona loginMiddleware

---
+++ middlewares/loginMeddlewares.js
@@ -0,0 +1,10 @@
+module.exports = (err, _req, res, _next) => {
+  console.log(err);
+  if (err.isJoi) return res.status(400).json({ message: err.details[0].message });
+
+  if (err.code === 'invalidLogin') {
+    return res.status(409).json({ message: 'Invalid fields' });
+  }
+
+  return res.status(500).json({ message: 'critical error :(' });
+};
