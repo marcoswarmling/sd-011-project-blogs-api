c9b15851635f9ce6884cf6e91b9a9b4aa91c0c59
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-25 23:17:09 -0300

    feat: add category name validation to route

--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -3,9 +3,10 @@ const express = require('express');
 const categoryController = require('../controllers/categoryController');
 
 const authValidator = require('../middlewares/validations/auth/authValidator');
+const categoryValidator = require('../middlewares/validations/category/categoryValidator');
 
 const categoryRouter = express.Router();
 
-categoryRouter.post('/', authValidator, categoryController.create);
+categoryRouter.post('/', authValidator, categoryValidator, categoryController.create);
 
 module.exports = categoryRouter;
