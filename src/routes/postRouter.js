9ac2e372c37873701ca44607db8399b7e5e5ee7b
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-26 14:28:50 -0300

    feat: add post validation middleware to route

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -3,9 +3,10 @@ const express = require('express');
 const postController = require('../controllers/postController');
 
 const authValidator = require('../middlewares/validations/auth/authValidator');
+const postValidator = require('../middlewares/validations/post/postValidator');
 
 const postRouter = express.Router();
 
-postRouter.post('/', authValidator, postController.create);
+postRouter.post('/', authValidator, postValidator, postController.create);
 
 module.exports = postRouter;
