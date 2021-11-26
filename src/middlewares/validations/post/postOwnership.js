e17723e1b3ab0a7025c477e0fd7cc929722e9003
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-26 19:33:04 -0300

    feat: add post ownership middleware

---
+++ src/middlewares/validations/post/postOwnership.js
@@ -0,0 +1,13 @@
+const errors = require('../../../schemas/errorsSchema');
+const { getPostBasicInfo } = require('../../../services/postService');
+
+module.exports = async (request, _response, next) => {
+  const { user, params } = request;
+  const { id } = params;
+
+  const post = await getPostBasicInfo(id);
+
+  if (post.userId !== user.id) return next(errors.post.unauthorizedUser);
+
+  return next();
+};
