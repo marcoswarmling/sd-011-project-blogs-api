ac9513230a44719691eeb191e04d64544600fa3e
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-11-26 15:50:17 -0300

    build: create route categories

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const userRouter = require('./src/routes/userRoutes');
 const loginRouter = require('./src/routes/loginRouter');
+const categoryRouter = require('./src/routes/categoryRouter');
 
 const app = express();
 app.use(express.json());
 
 app.use('/user', userRouter);
 app.use('/login', loginRouter);
+app.use('/categories', categoryRouter);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
---
+++ src/routes/categoryRouter.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+// const categoryController = require('../controller/category');
+
+// router.post('/', categoryController.getLogIn);
+
+module.exports = router;
