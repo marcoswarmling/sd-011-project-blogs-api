36518c76e6f4bb4e120c0942258b87d12d7596ab
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-25 22:39:34 -0300

    feat: add auth validator middleware

---
+++ src/middlewares/validations/auth/authValidator.js
@@ -0,0 +1,18 @@
+const errors = require('../../../schemas/errorsSchema');
+const { validateToken } = require('../../../helpers/jwt');
+
+module.exports = (req, _res, next) => {
+  const { authorization: token } = req.headers;
+
+  if (!token) return next(errors.auth.notFound);
+
+  try {
+    const data = validateToken(token);
+
+    req.user = data;
+
+    return next();
+  } catch (error) {
+    return next(errors.auth.invalidJWT);
+  }
+};
