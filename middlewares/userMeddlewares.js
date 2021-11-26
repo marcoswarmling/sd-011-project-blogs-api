dc9b1f2697a5e974ef39277455f6e169d2357259
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-25 22:19:14 -0300

    retira switch case de middleware

--- middlewares/userMeddlewares.js
+++ middlewares/userMeddlewares.js
@@ -1,14 +1,17 @@
 module.exports = (err, _req, res, next) => {
-  switch (err) {
-    case err.isJoi:
-      return res.status(400).json({ message: err.details[0].message });
-    case err.code === 'conflict':
-      return res.status(409).json({ message: 'User already registered' });
-    case err.code === 'invalidEmail':
-      return res.status(400).json({ message: 'email must be a valid email' });
-    case err.code === 'invalidPass':
-      return res.status(400).json({ message: 'password length must be 6 characters long' });
-    default:
-      return next(err);
+  if (err.isJoi) return res.status(400).json({ message: err.details[0].message });
+
+  if (err.code === 'conflict') {
+    return res.status(409).json({ message: 'User already registered' });
   }
+
+  if (err.code === 'invalidEmail') {
+    return res.status(400).json({ message: 'email must be a valid email' });
+  }
+
+  if (err.code === 'invalidPass') {
+    return res.status(400).json({ message: 'password length must be 6 characters long' });
+  }
+
+  return next(err);
 };
--- middlewares/userMiddlewaresComplement.js
+++ middlewares/userMiddlewaresComplement.js
@@ -1,12 +1,10 @@
 module.exports = (err, _req, res, _next) => {
-  switch (err) {
-    case err.code === 'invalidName':
-      return res.status(400).json({ 
-        message: 'displayName length must be at least 8 characters long',
-      });
-    default:
-      return res.status(500).json({ 
-        message: 'critical error :(',
-      });
+  if (err.code === 'invalidName') {
+    return res.status(400).json({ 
+      message: 'displayName length must be at least 8 characters long',
+    });
   }
+  return res.status(500).json({ 
+    message: 'critical error :(',
+  });
 };
