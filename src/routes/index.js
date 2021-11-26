ed5f1fe449becf3ca2ccf06211417e40dad19aa2
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-26 13:27:27 -0300

    feat: add post router

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const userRouter = require('./userRouter');
 const loginRouter = require('./loginRouter');
 const categoryRouter = require('./categoryRouter');
+const postRouter = require('./postRouter');
 
 const rootRouter = express.Router();
 
 rootRouter.use('/user', userRouter);
 rootRouter.use('/login', loginRouter);
 rootRouter.use('/categories', categoryRouter);
+rootRouter.use('/post', postRouter);
 
 module.exports = rootRouter;
