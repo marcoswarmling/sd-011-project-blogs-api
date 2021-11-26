a3e58b5facf3871918066d759d112ffe25e83173
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-11-26 16:33:34 -0300

    refactor: implement getallCategories fucntion

--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const categoryController = require('../controller/category');
 const { verifyToken } = require('../middleware/token');
 
-router.post('/', verifyToken, categoryController.createCategory);
+router.use(verifyToken);
+router.post('/', categoryController.createCategory);
+router.get('/', categoryController.getAllCategories);
 
 module.exports = router;
