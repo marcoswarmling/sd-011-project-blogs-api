412daf51e2366df3e985eff93f572e7e4be62632
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-25 22:41:43 -0300

    feat: add auth validator middleware to get all users route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,10 +3,11 @@ const express = require('express');
 const userController = require('../controllers/userController');
 
 const userValidator = require('../middlewares/validations/user/userValidator');
+const authValidator = require('../middlewares/validations/auth/authValidator');
 
 const userRouter = express.Router();
 
 userRouter.post('/', userValidator, userController.create);
-userRouter.get('/', userController.getAll);
+userRouter.get('/', authValidator, userController.getAll);
 
 module.exports = userRouter;
