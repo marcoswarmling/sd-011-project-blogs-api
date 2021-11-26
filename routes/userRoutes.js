b14f4e0f0c486499b7c3791527cd3c41c8c53ab6
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-11-26 15:05:21 -0300

    build(getallusers): implement getAllUsers and it`s route
    To prevent possible uses I create token middleware where verify if the inserted token is valid and
    return the user information if require

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userController = require('../src/controller/user');
+const { verifyToken } = require('../src/middleware/token');
 
 router.post('/', userController.createNewUser);
+router.get('/', verifyToken, userController.getAllUser);
 
 module.exports = router;
