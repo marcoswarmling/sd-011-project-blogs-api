fd68897315a5705d31f6788abd4742d9a07326a6
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-26 11:51:51 -0300

    cria utils para validaçoes

---
+++ utils/validations.js
@@ -0,0 +1,52 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { User } = require('../models');
+
+// lógica adaptada de https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+const validEmail = (email) => {
+  if (email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/igm)) return true;
+  return false;
+};
+
+const validName = (name) => {
+  if (name.length >= 8) return true;
+  return false;
+};
+
+const validPass = (pass) => {
+  if (pass.length === 6) return true;
+  return false;
+};
+
+const existingUser = async (email) => {
+  const user = await User.findOne({ where: { email } });
+  // console.log(user);
+  return user;
+};
+
+const tokenGenerator = (displayName, email, password, image) => {
+  const secret = process.env.JWT_SECRET;
+  
+  const jwtConfig = {
+    expiresIn: '1h',
+    algorithm: 'HS256',
+  };
+
+  const payload = {
+    displayName,
+    email,
+    password,
+    image,
+  };
+
+  const token = jwt.sign({ data: payload }, secret, jwtConfig);
+  return token;
+};
+
+module.exports = {
+  validEmail,
+  validName,
+  validPass,
+  tokenGenerator,
+  existingUser,
+};
