4f4c78649032b4cd1aa887fed7253ea0c1676b11
Wilquer82 <wilquer.fig@gmail.com>
2021-11-26 16:58:37 -0300

    Estrutura e config

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 
+const usersRoute = require('./routes/usersRoute');
+const loginRoute = require('./routes/loginRoute');
+const blogpostsRoute = require('./routes/blogpostsRoute');
+const categoriesRoute = require('./routes/categoriesRoute');
+
 const app = express();
+
+app.use(express.json());
+app.use('/user', usersRoute);
+app.use('/login', loginRoute);
+app.use('/post', blogpostsRoute);
+app.use('/categories', categoriesRoute);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
