fb2d2d5cfab2825bbc2093bdbd548e2417292a77
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-26 18:43:23 -0300

    refactor: validate post to validate new psot attributes

--- src/helpers/postHelper.js
+++ src/helpers/postHelper.js
@@ -1,11 +1,13 @@
 const errors = require('../schemas/errorsSchema');
 
-const expectedFields = ['title', 'categoryIds', 'content'];
+const attributes = {
+  new: ['title', 'categoryIds', 'content'],
+};
 
-const validatePost = (post) => {
-  expectedFields.forEach((field) => {
+const validateNewPostAttributes = (post) => {
+  attributes.new.forEach((field) => {
     if (!post[field]) throw errors.requiredField(field);
   });
 };
 
-module.exports = { validatePost };
+module.exports = { validateNewPostAttributes };
