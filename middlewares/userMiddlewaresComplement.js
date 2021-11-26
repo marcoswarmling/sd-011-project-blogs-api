8f6b235ee9cfe2eed415e269b5b5c31adf3954b5
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-26 17:56:48 -0300

    adiciona condiçao de erro para invalidId

--- middlewares/userMiddlewaresComplement.js
+++ middlewares/userMiddlewaresComplement.js
@@ -7,7 +7,9 @@ module.exports = (err, _req, res, _next) => {
   if (err.code === 'missingAuthToken') {
     return res.status(401).json({ message: 'Token not found' });
   }
-
+  if (err.code === 'invalidId') {
+    return res.status(404).json({ message: 'User does not exist' });
+  }
   return res.status(500).json({ 
     message: 'critical error :(',
   });
