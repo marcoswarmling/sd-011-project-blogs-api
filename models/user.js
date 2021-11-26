725479f73e4cb69b65e373063974a19e2cdfa37f
theusseveen22 <mateus.krauss@gmail.com>
2021-11-26 13:12:30 -0300

    Adiciona configuraçoes do sequelize

---
+++ models/user.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.INTEGER,
+    image: DataTypes.INTEGER,
+  },
+  {
+    timestamps: false, // remove a obrigatoriedade de utilizar os campos `createdAt` e `updatedAt`
+ });
+
+  return User;
+};
