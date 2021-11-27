c251c024c3df5755b4b58e7605091903c7b72a39
Bruno Duarte <brunoarduarte@gmail.com>
2021-11-27 17:02:04 -0300

    Requisito 3

---
+++ middlewares/tokenValidation.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const db = require('../models');
+
+const tokenValidation = async (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+  try {
+    const decodedToken = jwt.verify(token, process.env.secret);
+        
+    const checkTokenAlreadyExists = await db.Users.findOne({
+      where: { email: decodedToken.login.email },
+    });
+    
+    if (!checkTokenAlreadyExists) {
+      return res.status(401).json({ message: 'Token not found' });
+    }
+   next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
+
+module.exports = tokenValidation;
--- routes/user.js
+++ routes/user.js
@@ -5,11 +5,12 @@ const {
   nameLengthValidation,
   passwordValidation,
   checkEmailAlreadyRegistered } = require('../middlewares/usersValidations');
+const tokenValidation = require('../middlewares/tokenValidation');
 
 const validations = [emailValidation, nameLengthValidation,
    passwordValidation, checkEmailAlreadyRegistered];
 
-router.get('/', UserController.getAllUsers);
+router.get('/', tokenValidation, UserController.getAllUsers);
 router.post('/', validations, UserController.createUser);
 
 module.exports = router;
