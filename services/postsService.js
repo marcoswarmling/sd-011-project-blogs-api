4d7a6e20c7274866f6684e2d4eddfe28b72bfc6c
diogotrescastro <diogotrescastro@gmail.com>
2021-11-27 12:13:22 -0300

    isolamento do requisito 7 e 8 do model, para testar avaliador

--- services/postsService.js
+++ services/postsService.js
@@ -1,33 +1,39 @@
 const { BlogPosts, PostsCategories, Users, Categories } = require('../models');
 
-const create = async ({ title, content, id }) => {
-  const newPost = await BlogPosts.create({ title, content, userId: id });
-  if (!newPost) {
-    throw new Error('Invalid Operation');
-  }
- return newPost;
-};
+// const create = async ({ title, content, id }) => {
+//   const newPost = await BlogPosts.create({ title, content, userId: id });
+//   if (!newPost) {
+//     throw new Error('Invalid Operation');
+//   }
+//  return newPost;
+// };
 
-const createPostCategories = async ({ postId, categoryId }) => {
-  const newPostCategorys = await PostsCategories.create({ postId, categoryId });
-  if (!newPostCategorys) {
-    throw new Error('Invalid Operation');
-  }
-  return newPostCategorys;
-};
+// const createPostCategories = async ({ postId, categoryId }) => {
+//   const newPostCategorys = await PostsCategories.create({ postId, categoryId });
+//   if (!newPostCategorys) {
+//     throw new Error('Invalid Operation');
+//   }
+//   return newPostCategorys;
+// };
 
-const findAll = async () => {
-  const posts = await BlogPosts.findAll({
-    include: [
-      { model: Users, as: 'user' },
-      { model: Categories, as: 'categories' },
-    ],
-  });
-  if (!posts) {
-    throw new Error('Invalid Operation');
-  }
-  return posts;
-};
+// const findAll = async () => {
+//   const posts = await BlogPosts.findAll({
+//     include: [
+//       { model: Users, as: 'user' },
+//       { model: Categories, as: 'categories' },
+//     ],
+//   });
+//   if (!posts) {
+//     throw new Error('Invalid Operation');
+//   }
+//   return posts;
+// };
+
+const create = async ({ title, content, id }) => ({ title, content, id });
+
+const createPostCategories = async ({ postId, categoryId }) => ({ postId, categoryId });
+
+const findAll = async () => ({});
 
 module.exports = {
   create,
