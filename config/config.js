e064b8ae532b04f6c55f4ef1ccaff0d301c1d6e1
Jean Esteves <jean.esteves2001@gmail.com>
2021-11-27 10:37:46 -0300

    fix: tabs lint error

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
 
 module.exports = {
-	development: {
-		username: process.env.MYSQL_USER,
-		password: process.env.MYSQL_PASSWORD,
-		database: 'blogs_api',
-		host: process.env.MYSQ_HOST_NAME,
-		dialect: 'mysql',
-	},
-	test: {
-		username: process.env.MYSQL_USER,
-		password: process.env.MYSQL_PASSWORD,
-		database: 'blogs_api',
-		host: process.env.MYSQ_HOST_NAME,
-		dialect: 'mysql',
-	},
-	production: {
-		username: process.env.MYSQL_USER,
-		password: process.env.MYSQL_PASSWORD,
-		database: 'blogs_api',
-		host: process.env.MYSQ_HOST_NAME,
-		dialect: 'mysql',
-	},
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.MYSQ_HOST_NAME,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.MYSQ_HOST_NAME,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.MYSQ_HOST_NAME,
+    dialect: 'mysql',
+  },
 };
