551940d03508196e94a89349457617bd741732dd
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-27 13:28:43 -0300

    adiciona validaçao de categoria em service

--- services/postService.js
+++ services/postService.js
@@ -1,12 +1,17 @@
 const { BlogPost } = require('../models');
-const { isValidUser } = require('../utils/validations');
+const { isValidUser, isValidCategory } = require('../utils/validations');
 
 const postRegister = async (post, userEmail) => {
   const { title, content, categoryIds } = post;
 
-  const result = await isValidUser(userEmail);
-  if (!result.error) return BlogPost.create({ title, content, categoryIds });
-  return result;
+  const validCategoryOne = await isValidCategory(categoryIds[0]);
+  const validCategoryTwo = await isValidCategory(categoryIds[1]);
+
+  if (!validCategoryOne && !validCategoryTwo) {
+    const result = await isValidUser(userEmail);
+    if (!result.error) return BlogPost.create({ title, content, categoryIds });
+    return result;
+  } return ({ validCategoryOne, validCategoryTwo });
 };
 
 module.exports = {
