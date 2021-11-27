ed65b95c3371c3727ad84765312ae238ff577b5d
Savio Moraes <smmsavio@hotmail.com>
2021-11-27 09:49:47 -0300

    insere token, retorna token e altera os awaits por returns

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const { User } = require('../models');
+
+const secret = 'cookmaster';
+
+const jwtConfig = {
+  algorithm: 'HS256',
+};
 
 const router = express.Router();
 
@@ -7,8 +14,9 @@ router.post('/', async (req, res) => {
   try {
     const { displayName, email, password, image } = req.body;
     const newUser = await User.create({ displayName, email, password, image });
+    const token = jwt.sign({ data: newUser }, secret, jwtConfig);
 
-    return res.status(201).json(newUser);
+    return res.status(201).json(token);
   } catch (e) {
     console.log(e);
     return res.status(409).json({ message: 'User already registered' });
--- validations/validateUser.js
+++ validations/validateUser.js
@@ -2,7 +2,7 @@ const validateDisplayName = async (req, res, next) => {
   const { displayName } = req.body;
 
   if (displayName.length < 8) {
-    await res.status(400)
+    return res.status(400)
     .json({ message: '"displayName" length must be at least 8 characters long' });
   }
   next();
@@ -12,11 +12,11 @@ const validateEmal = async (req, res, next) => {
   const { email } = req.body;
 
   if (!email) {
-    await res.status(400).json({ message: '"email" is required' });
+    return res.status(400).json({ message: '"email" is required' });
   }
   const parseEmail = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
   if (parseEmail.test(email) === false) {
-    await res.status(400).json({ message: '"email" must be a valid email' });
+    return res.status(400).json({ message: '"email" must be a valid email' });
   }
 
   next();
@@ -25,10 +25,10 @@ const validateEmal = async (req, res, next) => {
 const verifyPassword = async (req, res, next) => {
   const { password } = req.body;
   if (!password) {
-    await res.status(400).json({ message: '"password" is required' });
+    return res.status(400).json({ message: '"password" is required' });
   }
   if (password.length !== 6) {
-    await res.status(400)
+    return res.status(400)
     .json({ message: '"password" length must be 6 characters long' });
   }
   next();
