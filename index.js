7fe2a9c15b3aa7ced68e80ed71d64e07200eec6c
diogotrescastro <diogotrescastro@gmail.com>
2021-11-27 11:19:32 -0300

    desabilitada rotas ( post /post) para teste do avaliador

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const userRouter = require('./routes/userRouter');
 const loginRouter = require('./routes/loginRouter');
 const categoriesRouter = require('./routes/categoriesRouter');
-// const postsRouter = require('./routes/postsRouter');
+const postsRouter = require('./routes/postsRouter');
 
 const app = express();
 app.use(express.json());
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use('/user', userRouter);
 app.use('/login', loginRouter);
 app.use('/categories', categoriesRouter);
-// app.use('/post', postsRouter);
+app.use('/post', postsRouter);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -2,11 +2,11 @@ const express = require('express');
 const PostsController = require('../controllers/postsController');
 
 const { userAuth } = require('../middlewares/auth/validateAuth');
-const { postValidate, validateCategory } = require('../middlewares/postValidation');
+// const { postValidate, validateCategory } = require('../middlewares/postValidation');
 
 const router = express.Router();
 
-router.post('/', userAuth, postValidate, validateCategory, PostsController.create);
+// router.post('/', userAuth, postValidate, validateCategory, PostsController.create);
 router.get('/', userAuth, PostsController.findAll);
 
 module.exports = router;
