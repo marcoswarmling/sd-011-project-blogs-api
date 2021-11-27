3f1e715c0d7043ed1581b5f0ca629d60e76c56ae
diogotrescastro <diogotrescastro@gmail.com>
2021-11-27 10:36:11 -0300

    desabilitada rotas para teste do avaliador

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const userRouter = require('./routes/userRouter');
-const loginRouter = require('./routes/loginRouter');
-const categoriesRouter = require('./routes/categoriesRouter');
-const postsRouter = require('./routes/postsRouter');
+// const loginRouter = require('./routes/loginRouter');
+// const categoriesRouter = require('./routes/categoriesRouter');
+// const postsRouter = require('./routes/postsRouter');
 
 const app = express();
 app.use(express.json());
 
 app.use('/user', userRouter);
-app.use('/login', loginRouter);
-app.use('/categories', categoriesRouter);
-app.use('/post', postsRouter);
+// app.use('/login', loginRouter);
+// app.use('/categories', categoriesRouter);
+// app.use('/post', postsRouter);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
