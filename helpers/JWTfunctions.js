a22cc32b669439ba1c4de2f6ac2984dad7cd486b
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-27 14:45:52 -0300

    Substitui senha por ID no JWT

--- helpers/JWTfunctions.js
+++ helpers/JWTfunctions.js
@@ -3,8 +3,8 @@ const jwt = require('jsonwebtoken');
 const JWT_CONFIG = { algorithm: 'HS256', expiresIn: '7d' };
 const secret = 'secret';
 
-const generateToken = ({ displayName, email, password, image }) => {
-  const data = { displayName, email, password, image };
+const generateToken = ({ displayName, email, id, image }) => {
+  const data = { displayName, email, id, image };
   const token = jwt.sign(data, secret, JWT_CONFIG);
   return token;
 };
