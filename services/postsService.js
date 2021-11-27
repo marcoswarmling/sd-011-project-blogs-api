c3cfe6fb042599d755494c61e7f1c11b5c70d070
diogotrescastro <diogotrescastro@gmail.com>
2021-11-27 13:06:10 -0300

    reintegrado os models

--- services/postsService.js
+++ services/postsService.js
@@ -1,39 +1,33 @@
-// const { BlogPosts, PostsCategories, Users, Categories } = require('../models');
+const { BlogPosts, PostsCategories, Users, Categories } = require('../models');
 
-// const create = async ({ title, content, id }) => {
-//   const newPost = await BlogPosts.create({ title, content, userId: id });
-//   if (!newPost) {
-//     throw new Error('Invalid Operation');
-//   }
-//  return newPost;
-// };
+const create = async ({ title, content, id }) => {
+  const newPost = await BlogPosts.create({ title, content, userId: id });
+  if (!newPost) {
+    throw new Error('Invalid Operation');
+  }
+ return newPost;
+};
 
-// const createPostCategories = async ({ postId, categoryId }) => {
-//   const newPostCategorys = await PostsCategories.create({ postId, categoryId });
-//   if (!newPostCategorys) {
-//     throw new Error('Invalid Operation');
-//   }
-//   return newPostCategorys;
-// };
+const createPostCategories = async ({ postId, categoryId }) => {
+  const newPostCategorys = await PostsCategories.create({ postId, categoryId });
+  if (!newPostCategorys) {
+    throw new Error('Invalid Operation');
+  }
+  return newPostCategorys;
+};
 
-// const findAll = async () => {
-//   const posts = await BlogPosts.findAll({
-//     include: [
-//       { model: Users, as: 'user' },
-//       { model: Categories, as: 'categories' },
-//     ],
-//   });
-//   if (!posts) {
-//     throw new Error('Invalid Operation');
-//   }
-//   return posts;
-// };
-
-const create = async ({ title, content, id }) => ({ title, content, id });
-
-const createPostCategories = async ({ postId, categoryId }) => ({ postId, categoryId });
-
-const findAll = async () => ({});
+const findAll = async () => {
+  const posts = await BlogPosts.findAll({
+    include: [
+      { model: Users, as: 'user' },
+      { model: Categories, as: 'categories' },
+    ],
+  });
+  if (!posts) {
+    throw new Error('Invalid Operation');
+  }
+  return posts;
+};
 
 module.exports = {
   create,
