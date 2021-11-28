481e1cf48055374e3cb2ab8a4be5f70faab96239
DarioJunior <dario.junior.rs@gmail.com>
2021-11-27 23:06:13 -0300

    Paste user model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+module.export = (sequelize, DataTypes) => {
+  const User = sequelize.model('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tableName: 'Users',
+    timestamps: false,
+  });
+
+  return User;
+};
