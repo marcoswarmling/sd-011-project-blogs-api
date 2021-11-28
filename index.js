2705419561e6d8b44906f7aae4b0095c24f942fc
DiegoCS777 <diegomethal@gmail.com>
2021-11-28 09:44:10 -0300

    iniciando projeto

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-
+// só pra commitar
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
