3b8379b92b374fec7df79054791413eabc64979b
Erick-Marinho <erickgaves@gmail.com>
2021-11-28 16:32:00 -0300

    ci(server): inserindo variáveis de ambientes

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log('Aplicação rodando na porta 3000!'));
