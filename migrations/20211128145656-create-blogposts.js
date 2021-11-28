08bf618f074730fe25471c68afd7b39782f569ab
jhenriquemargrafm <jh.margraf@mail.com>
2021-11-28 12:06:03 -0300

    migrations feitas

---
+++ migrations/20211128145656-create-blogposts.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPosts',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        title: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        content: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+        published: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updated: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+      }
+    );
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('BlogPosts');
+  }
+};
