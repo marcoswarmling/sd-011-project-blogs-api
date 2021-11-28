3a0c8eb4f08f5bd4e40c9726c8c5298e7a5322f0
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-28 12:16:06 -0300

    adiciona getPostById em router

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,5 +4,6 @@ const auth = require('../middlewares/authMiddleware');
 
 router.post('/post', auth, controller.postRegister);
 router.get('/post', auth, controller.getAllPosts);
+router.get('/post/:id', auth, controller.getPostById);
 
 module.exports = router;
