e43c0f93741d70e5988614bb7e389aabcb4f56fe
DarioJunior <dario.junior.rs@gmail.com>
2021-11-28 16:03:54 -0300

    Feat<Login Router>: Create complete login controller with service, and generate token

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,19 +1,20 @@
 const ApiError = require('../utils/ApiError');
 const HttpCodes = require('../utils/HttpCodes');
 const UserService = require('../services/user.services');
-// const generateJWT = require('../utils/generateJWT');
+const generateJWT = require('../utils/generateJWT');
 
 async function logIn(req, res, next) {
   try {
     const { email, password } = req.body;
-    console.log(email, password);
     const emailExists = await UserService.getUserByEmail(email);
 
     if (!emailExists || emailExists.password !== password) {
       return next(ApiError.invalidFields());
     }
 
-    return res.status(HttpCodes.code.OK).json(emailExists);
+    const { displayName } = emailExists.dataValues;
+    const token = generateJWT({ displayName, email });
+    return res.status(HttpCodes.code.OK).json(token);
   } catch (err) {
     console.log('ERRRRRRRRRRRRRRRRRRRRRRRRRRRRRROR', err);
     return next(ApiError.internalServerError());
