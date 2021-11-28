0651e44db85980917300ce0ac151392ed0a2ad0a
Ivaldo Souza <ivaldo.souza@datapar.com.br>
2021-11-28 09:27:34 -0300

    Ajustes feitos nas configuracoes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+require('dotenv').config();
+const boryParser = require('body-parser');
 
 const app = express();
 
-const PORT = 3000;
+app.use(boryParser);
 
-app.listen(PORT, () => console.log('ouvindo porta 3000!'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Ouvindo porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
