f6133f2c0ffbec2bebd16354c4b9044568fee128
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-29 12:45:09 -0300

    adicion removePost em routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,5 +6,6 @@ router.post('/post', auth, controller.postRegister);
 router.get('/post', auth, controller.getAllPosts);
 router.get('/post/:id', auth, controller.getPostById);
 router.put('/post/:id', auth, controller.updatePost);
+router.delete('/post/:id', auth, controller.removePost);
 
 module.exports = router;
