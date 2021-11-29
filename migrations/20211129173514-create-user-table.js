ca55f921dd6ba3c8f9792068d7afcfaa0108eb2e
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-11-29 14:46:47 -0300

    FEAT: Add sequelize configs and User Model

---
+++ migrations/20211129173514-create-user-table.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const Users = await queryInterface.createTable(
+      'Users',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        displayName: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          field: 'display_name'
+        },
+        email: {
+          allowNull: false,
+          unique: true,
+          type: Sequelize.STRING,
+        },
+        password: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        image: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        }
+      },
+    );
+
+    return Users;
+  },
+
+  down: async (queryInterface, _Sequelize) => queryInterface.dropTable('Users')
+};
---
+++ models/Users.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define(
+    'Users',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      displayName: DataTypes.STRING,
+      email: { type: DataTypes.STRING, unique: true },
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    { timestamps: false, tableName: 'Users', underscored: false },
+  );
+
+  return Users;
+};
