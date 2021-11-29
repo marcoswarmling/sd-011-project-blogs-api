4db05be83ff2d1aa4ce0537bf4bfe9845247d661
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-29 14:35:08 -0300

    adiciona searchByTerm em controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -60,10 +60,17 @@ const removePost = rescue(async (req, res, next) => {
   return res.status(204).send();
 });
 
+const searchByTerm = rescue(async (req, res, _next) => {
+  const { search } = req.query;
+  const result = await service.searchByTerm(search);
+  res.status(200).json(result);
+});
+
 module.exports = {
   postRegister,
   getAllPosts,
   getPostById,
   updatePost,
   removePost,
+  searchByTerm,
 };
