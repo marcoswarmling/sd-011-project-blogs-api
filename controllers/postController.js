b105b71f0a0d3f7d869db5088e731133c4e0f842
Josan Johnata <josanjohnata@gmail.com>
2021-11-29 15:12:13 -0300

    Corrigi o requisito 8

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,25 +6,30 @@ const router = express.Router();
 const blogPost = require('../services/postService');
 
 router.post('/', validateToken, async (req, res) => {
-  // try {
-    const { title, content, categoryIds } = req.body;
-    const newPost = await blogPost.createBlogPost(title, content, categoryIds);
-    const { id } = req.user;
+  const { title, content, categoryIds } = req.body;
+  const { id } = req.user;
+  const newPost = await blogPost.createBlogPost(title, content, categoryIds, id);
 
-    if (newPost.message === '"categoryIds" not found') {
-      return res.status(400).json(newPost);
-    }
+  if (newPost.message === '"categoryIds" not found') {
+    return res.status(400).json(newPost);
+  }
 
-    if (typeof newPost.message === 'string') {
-      return res.status(400).json(newPost);
-    }
+  if (typeof newPost.message === 'string') {
+    return res.status(400).json(newPost);
+  }
 
-    res.status(201).json({
-      id: newPost.id,
-        userId: id,
-        title: newPost.title,
-        content: newPost.content,
-    });
+  res.status(201).json({
+    id: newPost.id,
+    userId: id,
+    title: newPost.title,
+    content: newPost.content,
+  });
+});
+
+router.get('/', validateToken, async (_req, res) => {
+  const findAll = await blogPost.findAllBlogPosts();
+
+  return res.status(200).json(findAll);
 });
 
 module.exports = router;
