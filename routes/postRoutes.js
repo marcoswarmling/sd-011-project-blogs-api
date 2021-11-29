15662ff2718e49e5811d24fd8e43c17aa6177721
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-29 14:36:26 -0300

    adiciona searchByTerm em rotas

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -7,5 +7,6 @@ router.get('/post', auth, controller.getAllPosts);
 router.get('/post/:id', auth, controller.getPostById);
 router.put('/post/:id', auth, controller.updatePost);
 router.delete('/post/:id', auth, controller.removePost);
+router.get('/post/search', auth, controller.searchByTerm);
 
 module.exports = router;
