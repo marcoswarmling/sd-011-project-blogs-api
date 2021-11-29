a770eae96aa2743d82492b2450866da165c2d1df
heyset <c.inaciomatheus@gmail.com>
2021-11-29 18:09:34 -0300

    Adiciona codigo para tracer bullet

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const { User } = require('./src/models');
+
+app.post('/user', (req, res) => {
+  User.create(req.body)
+    .then((createdUser) => {
+      console.log(createdUser);
+      res.json({ token: 'tokenubauba' });
+    });
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
