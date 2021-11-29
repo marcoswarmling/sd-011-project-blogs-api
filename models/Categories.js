36c64458f2e12f4150073562228c06ebf458c58e
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-11-29 13:46:48 -0300

    models e migrations corrigidas

---
+++ models/Categories.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING,
+  },
+  {
+    tableName: 'Categories',
+    timestamps: false,
+  });
+
+  return Category;
+};
