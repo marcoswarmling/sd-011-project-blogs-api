746160a11890ecb9e916117f621b7c9b54029a13
Peryclesreis <perycles_reis@hotmail.com>
2021-11-29 20:53:27 -0300

    test

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
