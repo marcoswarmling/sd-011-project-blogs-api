f8cc9033738da2e2d2279ff441bc02578660d9a0
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-11-29 18:24:10 -0300

    erro na reversao

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const { postValidation } = require('../validations');
-const { createPost } = require('../controllers/postController');
+const { createPost, listPosts } = require('../controllers/postController');
 
 const router = express.Router();
 
 router.post('/', postValidation, createPost);
+router.get('/', listPosts);
 
 module.exports = router;
