19b3a3dc117da50fedaf1cb3bb4c0b7bcbf75bbc
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-11-29 18:59:13 -0300

    [Feat] Creates validation that it is not possible to register a user with an existing email

--- validations/email.js
+++ validations/email.js
@@ -1,5 +1,8 @@
 const Joi = require('joi');
 
+const { User } = require('../models');
+const err = require('./errors');
+
 const joiEmailSchema = Joi.object({
   email: Joi.string()
     .email()
@@ -20,4 +23,20 @@ const validEmail = (req, res, next) => {
   next();
 };
 
-module.exports = validEmail;
+const emailAlredyExists = async (req, res, next) => {
+  const { email } = req.body;
+
+  const query = await User.findAll({
+    attributes: ['email'],
+    where: { email },
+  });
+
+  if (query.length !== 0) return res.status(409).json(err.userAlreadyRegistered);
+
+  next();
+};
+
+module.exports = {
+  validEmail,
+  emailAlredyExists,
+};
