abb602e7894f3be4063a72e04a8e829f9b87558a
Michel Pereira <michelbecauro@gmail.com>
2021-11-29 23:38:16 -0300

    chore: Create Users table
    That is implemented by '20211130002421-create-user.js' migration file

---
+++ src/migrations/20211130002421-create-user.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const usersTable = await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      }
+    });
+
+  return usersTable;
+
+  },
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
