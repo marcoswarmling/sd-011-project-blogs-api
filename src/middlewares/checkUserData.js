1cc9deb5e75f6ccb255572a0b6aa777d4b3fd6c0
Felipe Buso <felipe_buso@hotmail.com>
2021-11-30 07:59:45 -0300

    refactor: POST /user validation middleware

--- src/middlewares/checkUserData.js
+++ src/middlewares/checkUserData.js
@@ -1,4 +1,17 @@
-const { checkBodyUserData } = require('../helpers/checkBodyUserData');
+const Joi = require('joi');
+
+const checkBodyUserData = async (displayName, email, password, image) => {
+  const userData = { displayName, email, password, image };
+  const schema = Joi.object().keys({
+    displayName: Joi.string().min(8).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().length(6).required(),
+    image: Joi.string(),
+  });
+  const { error } = schema.validate(userData);
+  if (!error) return false;
+  return error;
+};
 
 const validateUserBodyData = async (req, res, next) => {
   const { displayName, email, password, image } = req.body;
