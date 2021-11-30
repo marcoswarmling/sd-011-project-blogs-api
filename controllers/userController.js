9392d56c45d689206267a0a2b04f153cf05cf006
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-11-30 12:04:30 -0300

    [Refactor] Remove console.logs

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,6 @@ const userService = require('../services/userService');
 const registerUser = async (req, res) => {
   const { displayName, email, password, image } = req.body;
 
-  console.log('==> Passou pelo Controller <==');
-
   const response = await userService.registerUser(displayName, email, password, image);
 
   return res.status(201).json(response);
--- services/userService.js
+++ services/userService.js
@@ -1,8 +1,6 @@
 const { User } = require('../models');
 
 const registerUser = async (displayName, email, password, image) => {
-  console.log('==> Passou pelo Service <==');
-
   const result = await User.create(
     { displayName, email, password, image },
   );
