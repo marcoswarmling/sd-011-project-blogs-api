39547d166cb6aa885471367e188408746bb460b1
Ivaldo Souza <ivaldo.souza@datapar.com.br>
2021-11-30 08:44:15 -0300

    fix2

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const categoriesController = require('../controller/categoriesController');
+const postController = require('../controller/categoriesController');
 const { checkPost } = require('../middleware/post');
 const { authorizationToken } = require('../middleware/user');
 
-router.post('/post', authorizationToken, categoriesController.create);
+router.post('/post', checkPost, authorizationToken, postController.create);
 
 module.exports = router;
