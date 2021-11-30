550b1c76aa38b4a76a69645a83d8aea5fdc3ff64
fernando lasmar <f3rnandolasmar@gmail.com>
2021-11-29 23:00:26 -0300

    Iniciando o sequelize

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
@@ -9,4 +10,6 @@ app.get('/', (request, response) => {
   response.send();
 });
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
