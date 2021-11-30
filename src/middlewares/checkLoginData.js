c31d9b0715ebdead49b618a40ff43ea5241d57d9
Felipe Buso <felipe_buso@hotmail.com>
2021-11-30 12:25:52 -0300

    refactor: POST /login validation middleware

--- src/middlewares/checkLoginData.js
+++ src/middlewares/checkLoginData.js
@@ -1,4 +1,15 @@
-const { checkBodyLogin } = require('../helpers/checkBodyLogin');
+const Joi = require('joi');
+
+const checkBodyLogin = async (email, password) => {
+  const loginData = { email, password };
+  const schema = Joi.object().keys({
+    email: Joi.string().empty().email().required(),
+    password: Joi.string().empty().length(6).required(),
+  });
+  const { error } = schema.validate(loginData);
+  if (!error) return false;
+  return error;
+};
 
 const checkLogin = async (req, res, next) => {
   const { email, password } = req.body;
