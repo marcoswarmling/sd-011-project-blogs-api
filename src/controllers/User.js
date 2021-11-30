c35e189d20a47bcfce32ec5af90cc6ecf96f32ac
heyset <c.inaciomatheus@gmail.com>
2021-11-30 16:01:19 -0300

    Adiciona login no controller

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,12 +1,12 @@
 const { User } = require('../models');
 const { Schema } = require('../services/validation');
 const { sign } = require('../services/token');
-const { ConflictError } = require('../errors');
+const { ConflictError, ValidationError } = require('../errors');
 
 const create = (userDataInput) => {
   new Schema('createUser').validate(userDataInput);
 
-  return User.create(userDataInput)
+  return User.create(userDataInput) // TODO use hashing
     .then(({ dataValues }) => {
       const token = sign({ userId: dataValues.id });
       return { token, createdUser: dataValues };
@@ -20,6 +20,26 @@ const create = (userDataInput) => {
     });
 };
 
+const login = (credentialsInput) => {
+  new Schema('loginUser').validate(credentialsInput);
+
+  const { email, password } = credentialsInput;
+
+  return User.findOne({
+    where: { email },
+  })
+  .then((foundUser) => {
+    if (!foundUser) throw new ValidationError('Invalid fields'); // TODO use authorization error instead
+
+    if (foundUser.password !== password) throw new ValidationError('Invalid fields'); // TODO use hashing
+
+    const token = sign({ userId: foundUser.id });
+
+    return token;
+  });
+};
+
 module.exports = {
   create,
+  login,
 };
