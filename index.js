bcb788a0bb598da5a409c31d71e8a96188e0dd55
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-11-29 21:45:31 -0300

    [Refactor] Change the execution order

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.use(express.json());
 
 app.use('/user', userRouter);
 
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
-
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
