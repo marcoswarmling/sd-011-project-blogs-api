af8eb0d09062d748f3c171238d93a74ebe3fef52
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-11-30 12:06:29 -0300

    [Refactor] Change the localization errors file

---
+++ helpers/errors.js
@@ -0,0 +1,12 @@
+const userAlreadyRegistered = {
+  message: 'User already registered',
+};
+
+const invalidFields = {
+  message: 'Invalid fields',
+};
+
+module.exports = {
+  userAlreadyRegistered,
+  invalidFields,
+};
