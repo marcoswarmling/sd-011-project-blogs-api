d2a74fd0b7b69df83b842564f45ffc7eea9d4b6c
Michel Pereira <michelbecauro@gmail.com>
2021-11-30 01:11:01 -0300

    REQ_1: Implements MODEL: 'user'

--- src/api/app.js
+++ src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { User } = require('../models');
 
 const app = express();
 
@@ -8,7 +9,17 @@ app.get('/', (request, response) => {
   });
 // não remova esse endpoint, e para o avaliador funcionar
 
-app.get('/ping', (_req, res) => res.send('tá rodando de boa'));
+app.get('/ping', (_req, res) => res.send('tá rodando de boa')); // Para teste de rota
+
+app.get('/users', async (_req, res) => { // Para teste de model
+  try {
+    const result = await User.findAll();
+  
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: 'Deu ruim' });
+  }
+});
 
 app.all('*', (req, res) => res.status(404).send('Router not found'));
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,12 @@
-// 'use strict';
-// const {
-//   Model
-// } = require('sequelize');
-// module.exports = (sequelize, DataTypes) => {
-//   class User extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       // define association here
-//     }
-//   };
-//   User.init({
-//     displayName: DataTypes.STRING
-//   }, {
-//     sequelize,
-//     modelName: 'User',
-//   });
-//   return User;
-// };
+const User = (sequelize, DataTypes) => {
+  const Users = sequelize.define('User', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+  }, {
+    timestamps: false,
+  });
+  return Users;
+};
+
+module.exports = User;
