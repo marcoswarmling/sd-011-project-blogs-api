918256bbdb15149534bee903c084c4261fd16534
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-11-30 13:22:42 -0300

    [Feat] Create validation for 'jwt'

---
+++ validations/authToken.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const err = require('../helpers/errors');
+
+require('dotenv').config();
+
+const PRIVATE_KEY = process.env.JWT_SECRET || '27mr';
+
+const validToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) return res.status(401).json(err.tokenNotFound);
+
+  jwt.verify(token, PRIVATE_KEY, (error, _decoded) => {
+    if (error) return res.status(401).json(err.invalidToken);
+
+    next();
+  });
+};
+
+module.exports = validToken;
