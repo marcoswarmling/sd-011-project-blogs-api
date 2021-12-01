24927f5cf35baaa3dcda00bc95210e162d980714
hheloisa <hhackenhaar@gmail.com>
2021-12-01 12:03:09 -0300

    Feat: cria rota get /user

--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,6 @@ const usersController = require('../controllers/users');
 const { userMiddleware } = require('../Middlewares/userMiddleware');
 
 router.post('/', userMiddleware, usersController.createNewUser);
+router.get('/', usersController.findAllUsers);
 
 module.exports = router;
