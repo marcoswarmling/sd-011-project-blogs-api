9fe9086dc1110f3244c49ae15b48d98ac653b938
Daniel Leon <daniel.radialista@gmail.com>
2021-11-30 22:18:29 -0300

    reenviando req. 1 e corrigido o config.js

--- api/config/config.js
+++ api/config/config.js
@@ -1,27 +1,25 @@
 require('dotenv').config();
 
-const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DATABASE, HOSTNAME } = process.env;
-
 module.exports = {
   development: {
-    username: MYSQL_USERNAME,
-    password: MYSQL_PASSWORD,
-    database: MYSQL_DATABASE,
-    host: HOSTNAME,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
   test: {
-    username: MYSQL_USERNAME,
-    password: MYSQL_PASSWORD,
-    database: MYSQL_DATABASE,
-    host: HOSTNAME,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
   production: {
-    username: MYSQL_USERNAME,
-    password: MYSQL_PASSWORD,
-    database: MYSQL_DATABASE,
-    host: HOSTNAME,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
 };
