f906c889cc970c31f0e8bf54054edcb172182e42
Michel Pereira <michelbecauro@gmail.com>
2021-12-01 10:24:38 -0300

    REQ_5: Implements MODEL: category

--- src/models/category.js
+++ src/models/category.js
@@ -1,23 +1,10 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class category extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  category.init({
-    name: DataTypes.STRING
+const something = (sequelize, DataTypes) => {
+  const Categories = sequelize.define('Category', {
+    name: DataTypes.STRING,
   }, {
-    sequelize,
-    modelName: 'category',
+    timestamps: false,
   });
-  return category;
+  return Categories;
 };
+
+module.exports = something;
