cfac1013f498b57684139cb959919a990fff332a
hheloisa <hhackenhaar@gmail.com>
2021-12-01 18:04:54 -0300

    Feat: rota get /categories, com verificaçao de token

--- routes/categories.js
+++ routes/categories.js
@@ -4,5 +4,6 @@ const categoriesController = require('../controllers/categories');
 const { validateToken } = require('../Middlewares/authorizations');
 
 router.post('/', validateToken, categoriesController.createCategory);
+router.get('/', validateToken, categoriesController.getCategories);
 
 module.exports = router;
