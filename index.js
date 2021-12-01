79918e9b3474b006ec77eb8932772d62a56270e8
hheloisa <hhackenhaar@gmail.com>
2021-12-01 17:01:20 -0300

    Adiciona router de 'categories'

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const usersRoutes = require('./routes/users');
 const loginRoutes = require('./routes/login');
+const categoriesRoutes = require('./routes/categories');
 const { errorMiddleware } = require('./Middlewares/errorMiddleware');
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 
 app.use('/user', usersRoutes);
 app.use('/login', loginRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.use(errorMiddleware);
 
