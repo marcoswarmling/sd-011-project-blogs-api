faa9380d0e80304f7afcf40a1ebc215e54c73e86
hheloisa <hhackenhaar@gmail.com>
2021-12-01 18:05:12 -0300

    Feat: controller getCategories

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,18 +2,25 @@ const categoriesService = require('../services/categories');
 const { status, intServerError } = require('../Helpers/status&messages');
 
 const createCategory = async (req, res) => {
-  console.log('entrei no controller');
   try {
     const { name } = req.body;
     const newCategory = await categoriesService.createCategory(name);
     if (newCategory.status) {
       return res.status(newCategory.status).json({ message: newCategory.message });
     }
-    console.log('newCategory CONTROLLER', newCategory);
     return res.status(status.create).json(newCategory);
   } catch (error) {
     return res.status(status.intServerError).json({ message: intServerError.unknown });
   }
 };
 
-module.exports = { createCategory };
+const getCategories = async (req, res) => {
+  try { 
+    const allCategories = await categoriesService.getAllCategories();
+    return res.status(status.sucess).json(allCategories);
+  } catch (error) {
+    return res.status(status.intServerError).json({ message: intServerError.unknown });
+  }
+};
+
+module.exports = { createCategory, getCategories };
