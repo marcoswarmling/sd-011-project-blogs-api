13e7027e97c1f601eb692177f45455f771bd91bf
guinhoanjos <iago.anjos@hotmail.com>
2021-12-01 12:10:05 -0300

    criado middlewares de validacao

---
+++ middlewares/validateEntries.js
@@ -0,0 +1,39 @@
+const {
+  passwordExists,
+  passwordLength,
+  emailExists,
+  displayNameLength,
+  isEmail,
+} = require('../services/verifyEntries');
+
+function validateEntries(req, res, next) {
+  const { displayName, email, password } = req.body;
+
+  if (!displayNameLength(displayName)) {
+    return res.status(400).json({ 
+      message: '"displayName" length must be at least 8 characters long',
+    });
+  }
+
+  if (!passwordLength(password)) {
+    return res.status(400).json({ message: '"password" length must be 6 characters long' });
+  }
+  if (!isEmail(email)) {
+    return res.status(400).json({ message: '"email" must be a valid email' });
+  }
+  next();
+}
+
+function entriesExists(req, res, next) {
+  const { email, password } = req.body;
+
+  if (!passwordExists(password)) res.status(400).json({ message: '"password" is required' });
+
+  if (!emailExists(email))res.status(400).json({ message: '"email" is required' });
+
+  next();
+}
+module.exports = {
+  validateEntries,
+  entriesExists,
+};
---
+++ services/verifyEntries.js
@@ -0,0 +1,40 @@
+const Joi = require('joi');
+
+function passwordLength(password) {
+if (password.length !== 6) return false;
+
+return true;
+}
+
+function passwordExists(password) {
+  if (!password) return false;
+  
+  return true;
+}
+
+function displayNameLength(displayName) {
+  if (displayName.length < 8) return false;
+    
+  return true;
+}
+
+function emailExists(email) {
+  if (!email) return false;
+
+  return true;
+}
+
+function isEmail(email) {
+  const schema = Joi.string().email();
+  const { error } = schema.validate(email);
+  if (error) return false;
+
+  return true;
+}
+module.exports = {
+  passwordExists,
+  passwordLength,
+  displayNameLength,
+  emailExists,
+  isEmail,
+};
