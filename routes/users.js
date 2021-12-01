d4df6db713e4e3e73c48ffece499aa6887b02ee0
Miguel Dantas <miiguelalb@gmail.com>
2021-12-01 17:12:26 -0300

    Concluindo req1

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,13 @@ const express = require('express');
 const routes = express.Router();
 
 const middlewares = require('../middlewares/users');
+const controllers = require('../controllers/users');
 
 routes.post('/',
   middlewares.displayNameValidation,
   middlewares.emailValidation,
   middlewares.passwordValidation,
-  middlewares.requiredPassword);
+  middlewares.requiredPassword,
+  controllers.createUser);
 
 module.exports = routes;
