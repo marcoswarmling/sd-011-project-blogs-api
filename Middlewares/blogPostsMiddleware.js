f22f95351d4b44ffe1ee340b3f9e541f64df67f4
hheloisa <hhackenhaar@gmail.com>
2021-12-02 17:26:51 -0300

    Feat: middleware de validaçao do body de blogPost

---
+++ Middlewares/blogPostsMiddleware.js
@@ -0,0 +1,17 @@
+const { status, postMessages } = require('../Helpers/status&messages');
+
+const postMiddleware = (req, res, next) => {
+  const { title, content, categoryIds } = req.body;
+  if (!title) {
+    return res.status(status.bad).json({ message: postMessages.titleRequired });
+  }
+  if (!content) {
+    return res.status(status.bad).json({ message: postMessages.contentRequired });
+  }
+  if (!categoryIds) {
+    return res.status(status.bad).json({ message: postMessages.categoryIdRequired });
+  }
+  next();
+};
+
+module.exports = { postMiddleware };
