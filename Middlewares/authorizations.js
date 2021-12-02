4505716e04f189091b923764d26953f3438f612c
hheloisa <hhackenhaar@gmail.com>
2021-12-02 17:23:49 -0300

    Fix: refatora middleware para adicionar as informaçoes do usuario em req.user

--- Middlewares/authorizations.js
+++ Middlewares/authorizations.js
@@ -1,15 +1,20 @@
 const jwt = require('jsonwebtoken');
+const { User } = require('../models');
 const { status, tokenMessages } = require('../Helpers/status&messages');
 
 const JWT = 'JWT_SECRET';
 
-const validateToken = (req, res, next) => {
+const validateToken = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) return res.status(status.unauth).json({ message: tokenMessages.notFound });
 
   try {
-  jwt.verify(authorization, JWT);
+  const { email } = jwt.verify(authorization, JWT);
+  
+  const user = await User.findOne({ where: { email } });
+  if (!user) return res.status(status.unauth).json({ message: tokenMessages.invalid });
 
+  req.user = user;
   next();
   } catch (error) {
     res.status(status.unauth).json({ message: tokenMessages.invalid });
