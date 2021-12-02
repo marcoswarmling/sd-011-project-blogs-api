0833bf59dd9bf08e3b6b00109f96f69b2b435042
heyset <c.inaciomatheus@gmail.com>
2021-12-02 18:39:24 -0300

    Adiciona codigo que cria categoria no banco e retorna

--- src/controllers/Category.js
+++ src/controllers/Category.js
@@ -1,10 +1,13 @@
 const { Category } = require('../models');
 const { Schema } = require('../services/validation');
 
+const getDisplayResultFromModelResult = ({ dataValues }) => dataValues;
+
 const create = (createCategoryInput) => {
   new Schema('createCategory').validate(createCategoryInput);
 
-  return Promise.resolve();
+  return Category.create(createCategoryInput)
+    .then(getDisplayResultFromModelResult);
 };
 
 module.exports = {
