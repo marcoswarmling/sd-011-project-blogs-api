00e117ee604268cba971274294751db0b0cd9dae
hheloisa <hhackenhaar@gmail.com>
2021-12-03 10:34:04 -0300

    Fix: refatora model de blogPost

--- models/blogPost.js
+++ models/blogPost.js
@@ -1,16 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
   const BlogPost = sequelize.define('BlogPost', { 
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    // id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     title: DataTypes.STRING,
     content: DataTypes.STRING,
-    published: DataTypes.DATE,
-    updated: DataTypes.DATE, 
-    userId: { type: DataTypes.INTEGER, foreignKey: true },
   }, {
+    createdAt: 'published',
+    updatedAt: 'updated', 
+    // userId: { type: DataTypes.INTEGER, foreignKey: true },
+  }); /* , {
     tableName: 'BlogPosts',
     timestamps: false,
-  });
-
+  }); */
   // Associação ao model de User
   BlogPost.associate = ({ User }) => {
     BlogPost.belongsTo(User, { foreignKey: 'userId', as: 'user' });
@@ -18,3 +18,7 @@ module.exports = (sequelize, DataTypes) => {
 
   return BlogPost;
 };
+
+/* Após o entendimento de que os erros estavam acontecendo pois não estavam sendo passados os dados corretamente,
+o model foi refatorado, com base no trabalho de Iago dos Anjos, PR: https://github.com/tryber/sd-011-project-blogs-api/pull/132/files
+dessa forma, o model só espera duas informações serem passadas, e pega automaticamente as infos disponibilizadas pelo migration */
