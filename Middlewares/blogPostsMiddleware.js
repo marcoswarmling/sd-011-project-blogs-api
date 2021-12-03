f169b40a1c882be7da0076a5430a8cb90acaacd1
hheloisa <hhackenhaar@gmail.com>
2021-12-03 19:07:59 -0300

    Feat: meddleware de verificaçao da rota PUT/post/:id

--- Middlewares/blogPostsMiddleware.js
+++ Middlewares/blogPostsMiddleware.js
@@ -14,4 +14,18 @@ const postMiddleware = (req, res, next) => {
   next();
 };
 
-module.exports = { postMiddleware };
+const putPostMiddleware = (req, res, next) => {
+const { title, content, categoryIds } = req.body;
+  if (!title) {
+    return res.status(status.badRequest).json({ message: postMessages.titleRequired });
+  }
+  if (!content) {
+    return res.status(status.badRequest).json({ message: postMessages.contentRequired });
+  }
+  if (categoryIds) {
+    return res.status(status.badRequest).json({ message: postMessages.noEditCategory });
+  }
+  next();
+};
+
+module.exports = { postMiddleware, putPostMiddleware };
