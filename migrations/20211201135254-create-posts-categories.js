2e809dfbd52c98e3c11a72ede260f67548ca0683
Ivaldo Souza <ivaldo.souza@datapar.com.br>
2021-12-03 11:36:22 -0300

    fix2

--- migrations/20211201135254-create-posts-categories.js
+++ migrations/20211201135254-create-posts-categories.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('postsCategories', {
+    await queryInterface.createTable('PostsCategories', {
       postId: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -20,6 +20,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('postsCategories');
+    await queryInterface.dropTable('PostsCategories');
   }
 };
