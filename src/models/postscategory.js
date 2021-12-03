befa55cbf276d723efe4d06af5083b8283038de8
Michel Pereira <michelbecauro@gmail.com>
2021-12-03 15:40:23 -0300

    REQ_7: Implements 'PostsCategory' MODEL

--- src/models/postscategory.js
+++ src/models/postscategory.js
@@ -1,24 +1,22 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class PostsCategory extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  PostsCategory.init({
-    postId: DataTypes.STRING,
-    categoryId: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'PostsCategory',
+module.exports = (sequelize, _DataTypes) => {
+  const postsCategory = sequelize.define('PostsCategory', {}, {
+    timestamps: false,
   });
-  return PostsCategory;
+  postsCategory.associate = (models) => {
+    models.BlogPost.belongsTo(models.Category, {
+      as: 'categories',
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+      through: 'postsCategory',
+    });
+
+    models.Category.belongsTo(models.BlogPost, {
+      as: 'blogPosts',
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+      through: 'postsCategory',
+    });
+  };
+
+  return postsCategory;
 };
