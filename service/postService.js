8c151f228a5202479943d579c03e80961d9da310
theusseveen22 <mateus.krauss@gmail.com>
2021-12-04 12:17:37 -0300

    Adiciona requisito 07 finalmente

--- service/postService.js
+++ service/postService.js
@@ -1,14 +1,17 @@
-const { BlogPosts, PostsCategories } = require('../models');
+const { BlogPosts, Categories } = require('../models');
 
 const ServiceInsertPost = async (id, title, content, categoryIds) => {
+    const userId = id;
     try {
-        const findPost = await PostsCategories.findAll({ raw: true });
-        const verifyIdsEqueal = findPost
-        .every((index) => categoryIds.includes(index));
-        if (!verifyIdsEqueal) {
+        const findPost = await Categories.findAll({ raw: true });
+        const ids = findPost.map((i) => i.id);
+        const verifyIdsEqueal = categoryIds
+        .every((element) => ids.includes(element));
+        console.log('VERIFY', verifyIdsEqueal);
+        if (!verifyIdsEqueal) { 
             return { error: 'notFound' };
         }
-        const insert = await BlogPosts.create({ userId: id, title, content });
+        const insert = await BlogPosts.create({ userId, title, content });
         return insert;
     } catch (error) {
         console.log(error);
