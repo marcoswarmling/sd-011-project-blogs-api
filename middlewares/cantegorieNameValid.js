b18d949f228f553a886b6e783a739005eaac95fb
theusseveen22 <mateus.krauss@gmail.com>
2021-12-04 13:39:07 -0300

    Adiciona return no middleware do name

--- middlewares/cantegorieNameValid.js
+++ middlewares/cantegorieNameValid.js
@@ -1,7 +1,7 @@
 const catNameValid = (req, res, next) => {
     const { name } = req.body;
     if (!name) {
-        res.status(400).json({
+        return res.status(400).json({
             message: '"name" is required',
         });
     }
