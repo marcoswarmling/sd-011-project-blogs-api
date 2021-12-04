9941b06a17f7a39dee945321f10e63f3c9970735
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-12-04 19:14:53 -0300

    feat: inicia sequelize

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
