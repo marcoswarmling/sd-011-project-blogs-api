0cf0148de3b9eda901ad159f9f6eddbd8acb6520
Matheus Costa <mathws.costag@gmail.com>
2021-12-04 07:38:39 -0300

    Cria as migrations

---
+++ migrations/20211204090242-Users.js
@@ -0,0 +1,33 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      displayName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    })
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
---
+++ migrations/20211204090308-Categories.js
@@ -0,0 +1,21 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+      },
+    })
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
