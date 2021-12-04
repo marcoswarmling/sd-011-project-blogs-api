440e465e5d25be3a356f05b1e8dbd2bd1aa1f5b1
hheloisa <hhackenhaar@gmail.com>
2021-12-03 22:27:18 -0300

    Feat: rota DELETE /post

--- routes/blogPosts.js
+++ routes/blogPosts.js
@@ -8,5 +8,6 @@ router.post('/', validateToken, postMiddleware, postController.createPost);
 router.get('/', validateToken, postController.getAllPosts);
 router.get('/:id', validateToken, postController.getById);
 router.put('/:id', validateToken, putPostMiddleware, verifyOwner, postController.editPost);
+router.delete('/:id', validateToken, verifyOwner, postController.deletePost);
 
 module.exports = router;
