396d64c104380d1b40ead42d85b6c89ce1defdc7
robsoncardozo <robsonleaocardozo@hotmail.com.br>
2021-12-04 19:14:39 -0300

    index.js express & Port implemented

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
+module.exports = app;
