6fb14e10286ab2ff816e76a4e80e1cd991863e94
hheloisa <hhackenhaar@gmail.com>
2021-12-03 22:27:46 -0300

    Fix: middlewareOwner, agora tambem verifica que o post existe

--- Middlewares/verifyOwner.js
+++ Middlewares/verifyOwner.js
@@ -1,18 +1,22 @@
 const { BlogPost } = require('../models');
-const { status, postMessages } = require('../Helpers/status&messages');
+const { status, postMessages, intServerError } = require('../Helpers/status&messages');
 
 const verifyOwner = async (req, res, next) => {
   try {
     const { id: postId } = req.params;
     const { id: idUser } = req.user;
     
-    const { dataValues } = await BlogPost.findOne({ where: { id: postId } });
-    if (dataValues.userId !== idUser) {
+    const findPost = await BlogPost.findOne({ where: { id: postId } });
+    if (!findPost) { 
+      return res.status(status.notFound).json({ message: postMessages.postNotExist }); 
+    }
+    const { userId } = findPost.dataValues;
+    if (userId !== idUser) {
       return res.status(status.unauth).json({ message: postMessages.unauthUser });
     }
     next();
   } catch (error) {
-    return res.status(status.intServerError).json({ message: postMessages.postNotExist });
+    return res.status(status.intServerError).json({ message: intServerError.unknown });
   }
 };
 
