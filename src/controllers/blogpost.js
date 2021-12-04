257effa4d35deab4c930a8980a1025d6328f7a32
Michel Pereira <michelbecauro@gmail.com>
2021-12-04 15:14:38 -0300

    REQ_7: Add  blogpost CONTROLLER: POST /post

--- src/controllers/blogpost.js
+++ src/controllers/blogpost.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { blogpost, category } = require('../services');
 const tokenValidMiddle = require('../validators/middlewares/tokenValidMiddle');
 
-// const STATUS_CREATED = 201;
+const STATUS_CREATED = 201;
 const STATUS_OK = 200;
 
 router.get('/', tokenValidMiddle, async (_req, res, next) => {
@@ -13,26 +13,26 @@ router.get('/', tokenValidMiddle, async (_req, res, next) => {
     res.status(STATUS_OK).json(result);
   });
 
-// const arrayIdsExists = async (categoryIds) => {
-//     const categories = await category.getAllByArrayIds(categoryIds);
-//     return categories;
-// };
+const arrayIdsExists = async (categoryIds) => {
+    const categories = await category.getAllByArrayIds(categoryIds);
+    return categories;
+};
 
-// const createPostsCategories = async (post, categories) => {
-//   post.addCategories(categories);
-// };
+const createPostsCategories = async (post, categories) => {
+  post.addCategories(categories);
+};
 
-// router.post('/', tokenValidMiddle, async (req, res, next) => {
-//     const { body, user } = req;
-//     const { categoryIds } = body;
+router.post('/', tokenValidMiddle, async (req, res, next) => {
+    const { body, user } = req;
+    const { categoryIds } = body;
 
-//     const categories = await arrayIdsExists(categoryIds);
+    const categories = await arrayIdsExists(categoryIds);
     
-//     const result = await blogpost.createIt({ body, user });
-//     if (result.message) return next(result);
-//     createPostsCategories(result, categories);
+    const result = await blogpost.createIt({ body, user });
+    if (result.message) return next(result);
+    createPostsCategories(result, categories);
     
-//     res.status(STATUS_CREATED).json(result);
-// });
+    res.status(STATUS_CREATED).json(result);
+});
 
 module.exports = router;
