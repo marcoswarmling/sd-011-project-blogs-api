57e70b74e28633ac37d12e5242e8ebb40d6fff90
robsoncardozo <robsonleaocardozo@hotmail.com.br>
2021-12-04 12:55:21 -0300

    dotenv added in config.js

--- config/config.js
+++ config/config.js
@@ -1,23 +1,32 @@
+require('dotenv').config();
+
+// syntax abbreviation
+const {
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  HOSTNAME,
+} = process.env;
+
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    username: MYSQL_USER,
+    password: MYSQL_PASSWORD,
     database: 'blogs_api',
-    host: process.env.HOSTNAME,
+    host: HOSTNAME,
     dialect: 'mysql',
   },
   test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    username: MYSQL_USER,
+    password: MYSQL_PASSWORD,
     database: 'blogs_api',
-    host: process.env.HOSTNAME,
+    host: HOSTNAME,
     dialect: 'mysql',
   },
   production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    username: MYSQL_USER,
+    password: MYSQL_PASSWORD,
     database: 'blogs_api',
-    host: process.env.HOSTNAME,
+    host: HOSTNAME,
     dialect: 'mysql',
   },
 };
