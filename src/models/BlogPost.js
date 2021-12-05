348268f85d828961fdfa9346ae6d494042694a6a
robsoncardozo <robsonleaocardozo@hotmail.com.br>
2021-12-05 13:19:03 -0300

    blogpost model added

---
+++ src/models/BlogPost.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+
+    // database fields:
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+
+  }, // options:
+  { tableName: 'BlogPosts', timestamps: false });
+
+    // associations:
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, { 
+      foreignKey: 'userId',
+      as: 'posts',
+    });
+  };
+
+  return BlogPost;
+};
--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
 
+    // database fields:
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
