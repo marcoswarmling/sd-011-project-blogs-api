5896c5c0d68a561f42658f9ca27f69ab838fe681
robsoncardozo <robsonleaocardozo@hotmail.com.br>
2021-12-05 10:39:20 -0300

    config updates

--- config/config.js
+++ config/config.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    // local test
-    // username: 'root',
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: 'blogs_api',
