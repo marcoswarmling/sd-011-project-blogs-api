9acfb26a071fc69ac95b6bdca810477815528d18
heyset <c.inaciomatheus@gmail.com>
2021-12-04 23:43:37 -0300

    Adiciona rota de get post by id

--- src/routers/post.js
+++ src/routers/post.js
@@ -20,4 +20,8 @@ router.get('/', validateToken, (_req, res, next) => {
     .catch(next);
 });
 
+router.get('/:id', validateToken, (req, res, next) => {
+  res.status(200).end();
+});
+
 module.exports = router;
