32ef475619bbfacd2607fab0f34cb631082f53a7
robsoncardozo <robsonleaocardozo@hotmail.com.br>
2021-12-05 10:27:05 -0300

    sequelize user created

--- config/config.js
+++ config/config.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 module.exports = {
   development: {
+    // local test
+    // username: 'root',
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: 'blogs_api',
---
+++ models/User.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User',
+  {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPosts, {
+      foreignKey: 'userId',
+      as: 'posts',
+    });
+  };
+
+  return User;
+};
