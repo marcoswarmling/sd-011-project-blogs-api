138b90a1896173d862ca961f5e6bd0f1bf524d4c
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-12-05 15:16:21 -0300

    REQ01: criando endpoint /user

--- src/models/Blogpost.js
+++ src/models/Blogpost.js
@@ -8,12 +8,7 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   BlogPost.associate = (models) => {
-<<<<<<< HEAD
     BlogPost.belongsTo(models.Users, { foreignKey: 'userId', as: 'user' });
-=======
-    BlogPost.belongsTo(models.User, { foreignKey: 'userId', as: 'users' });
-    BlogPost.hasMany(models.PostsCategory, { foreignKey: 'postId', as: 'PostsCategories' });
->>>>>>> 71741190d5f2a6695c0cc8e682e7b2659f5f7ca9
   };
   return BlogPost;
 };
