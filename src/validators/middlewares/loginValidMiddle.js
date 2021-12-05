78c4396b92fd22e7594d2e6bf3aa5003e0aa645e
Michel Pereira <michelbecauro@gmail.com>
2021-12-04 23:08:35 -0300

    chore: Implements 'notMiddleware/loginValid' REMOVING
    Tha means loginValid content is merged into 'loginValidMiddle' file

--- src/validators/middlewares/loginValidMiddle.js
+++ src/validators/middlewares/loginValidMiddle.js
@@ -1,4 +1,65 @@
-const { loginValidator } = require('../notMiddleware/loginValid');
+const { User } = require('../../models');
+
+const STATUS_BAD_REQUEST = 400;
+const MSG_MISSING_EMAIL = '"email" is required';
+const MSG_MISSING_PASSW = '"password" is required';
+const MSG_EMPTY_EMAIL = '"email" is not allowed to be empty';
+const MSG_EMPTY_PASSW = '"password" is not allowed to be empty';
+const MSG_INVALID_FIELDS = 'Invalid fields';
+
+function passwordValidator(password, userData) {
+  if (typeof password === 'undefined') {
+    return { status: STATUS_BAD_REQUEST, message: MSG_MISSING_PASSW };
+  }
+  
+  if (!password) {
+    return { status: STATUS_BAD_REQUEST, message: MSG_EMPTY_PASSW };
+  }
+  
+  if (password !== userData.password) {
+    return { status: STATUS_BAD_REQUEST, message: MSG_INVALID_FIELDS };
+  }
+
+  return {};
+}
+
+async function emailValidator(email) {
+  if (typeof email === 'undefined') {
+    return { status: STATUS_BAD_REQUEST, message: MSG_MISSING_EMAIL };
+  }
+  
+  if (!email) {
+    return { status: STATUS_BAD_REQUEST, message: MSG_EMPTY_EMAIL };
+  }
+    
+  const userData = await User.findOne({ where: { email } });
+
+  if (userData === null) {
+    return { status: STATUS_BAD_REQUEST, message: MSG_INVALID_FIELDS };
+  }
+
+  return userData;
+}
+
+const loginValidator = async (email, password) => {
+  const emailResult = await emailValidator(email);
+
+  if (emailResult.status) {
+    const { status, message } = emailResult;
+    return { status, message };
+  }
+
+  const passResult = passwordValidator(password, emailResult); // Not asked by Trybe "functional requirements", but I did implemented it
+
+  if (passResult.status) {
+    const { status, message } = passResult;
+    return { status, message };
+  }
+
+  const { id, displayName } = emailResult;
+
+  return { id, displayName };
+};
 
 module.exports = async (req, res, next) => {
   const { email, password } = req.body;
