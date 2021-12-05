a90ebf42904980bcb8ca9cc9eb7cb3d23dcc0e8f
Michel Pereira <michelbecauro@gmail.com>
2021-12-04 22:25:49 -0300

    chore: Implements 'error middleware' use in User CONTROLLER

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,40 +4,34 @@ const userValidMiddle = require('../validators/middlewares/userValidMiddle');
 const tokenValidMiddle = require('../validators/middlewares/tokenValidMiddle');
 
 const STATUS_CREATED = 201;
+const STATUS_OK = 200;
 
-router.get('/', tokenValidMiddle, async (_req, res) => {
+router.get('/', tokenValidMiddle, async (_req, res, next) => {
   const result = await user.getAll();
 
-  if (result.message) {
-    return res.status(500).json({ message: `Error in Controller. Error.msg: ${result.message}` });
-  }
+  if (result.message) return next(result);
 
-  res.status(200).json(result);
+  res.status(STATUS_OK).json(result);
 });
 
-router.post('/', userValidMiddle, async (req, res) => {
+router.post('/', userValidMiddle, async (req, res, next) => {
   const { body } = req;
 
   const result = await user.createIt(body);
 
-  if (result.message) {
-    return res.status(500).json({ message: `Error in Controller. Error.msg: ${result.message}` });
-  }
+  if (result.message) return next(result);
 
   res.status(STATUS_CREATED).json(result);
 });
 
-router.get('/:id', tokenValidMiddle, async (req, res) => {
+router.get('/:id', tokenValidMiddle, async (req, res, next) => {
   const { id } = req.params;
 
   const result = await user.getById(id);
 
-  if (result.message) {
-    const { status, message } = result;
-    return res.status(status).json({ message });
-  }
+  if (result.message) return next(result);
 
-  res.status(200).json(result);
+  res.status(STATUS_OK).json(result);
 });
 
 module.exports = router;
