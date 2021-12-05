268d5bd8f65ca28c25701bf6f8b9645b240fbff9
anaclarabck <ackyotoku@gmail.com>
2021-12-04 21:36:04 -0300

    refactor: req1

--- validations/userValidations.js
+++ validations/userValidations.js
@@ -8,30 +8,29 @@ const name = (req, res, next) => {
 };
 
 const email = (req, res, next) => {
-  const { email: vEmail } = req.body;
+  const user = req.body;
   const messageRequired = '"email" is required';
-  if (!vEmail) return res.status(400).json({ message: messageRequired });
+  if (!user.email) return res.status(400).json({ message: messageRequired });
   const re = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
   const messageValid = '"email" must be a valid email';
-  if (!re.test(vEmail)) return res.status(400).json({ message: messageValid });
+  if (!re.test(user.email)) return res.status(400).json({ message: messageValid });
   next();
 };
 
 const emailUnique = async (req, res, next) => {
-  const { email: vEmail } = req.body;
-  const users = await Users.findAll({ where: { email: vEmail } });
-  // console.log('users', users);
+  const user = req.body;
+  const users = await Users.findAll({ where: { email: user.email } });
   const message = 'User already registered';
   if (users.length > 0) return res.status(409).json({ message });
   next();
 };
 
 const password = (req, res, next) => {
-  const { password: vPassword } = req.body;
+  const user = req.body;
   const messageRequired = '"password" is required';
-  if (!vPassword) return res.status(400).json({ message: messageRequired });
+  if (!user.password) return res.status(400).json({ message: messageRequired });
   const messageValid = '"password" length must be 6 characters long';
-  if (vPassword.length !== 6) return res.status(400).json({ message: messageValid });
+  if (user.password.length !== 6) return res.status(400).json({ message: messageValid });
   next();
 };
 
