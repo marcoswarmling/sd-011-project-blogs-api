5447ec60bcd22df2fef9ba64a2f4b32078f20ce4
Italo <italomatos764@gmail.com>
2021-12-05 02:04:36 -0400

    Adicionando colunas

--- migrations/20211205055458-create-blog-post.js
+++ migrations/20211205055458-create-blog-post.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('BlogPosts', {
+    return queryInterface.createTable('BlogPosts', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -11,17 +11,29 @@ module.exports = {
       title: {
         type: Sequelize.STRING
       },
+      content: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: { model:'Users', key: 'id' }
+      },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        field: 'published',
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        field: 'updated',
       }
     });
   },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('BlogPosts');
-  }
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('BlogPosts');
+  },
 }; 
