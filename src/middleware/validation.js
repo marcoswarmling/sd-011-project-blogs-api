cb6ac7512a92a74a03ebd698fb46ff06e3021960
Paulo Freire <paulofreire.si@outlook.com>
2021-12-06 05:06:10 -0300

    added email verification func

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const { user } = require('../../models');
 
 const newUser = (name, email, password) => {
   const { error } = Joi.object({
@@ -10,6 +11,17 @@ const newUser = (name, email, password) => {
   if (error) throw error;
 };
 
+const alreadyExist = (email) => {
+  const exists = user.findOne({ where: { email } });
+  if (exists) {
+    const error = new Error('User already registered');
+    error.code = 409;
+    throw error;
+  }
+  return null;
+};
+
 module.exports = {
     newUser,
+    alreadyExist,
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,7 +4,7 @@ const { createToken } = require('../middleware/jwt');
 
 const newUser = async (displayName, email, password, image) => {
     validate.newUser(displayName, email, password);
-  //  await validate.alreadyExist(email);
+    await validate.alreadyExist(email);
     const result = user.create({ displayName, email, password, image });
     const token = createToken(result);
     return token;
