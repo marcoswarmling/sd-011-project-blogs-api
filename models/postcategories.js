7c5b844e0dc1e7572979f6da6fa6150fbb9ef06e
CamilaCode7 <camilafabiabh9@gmail.com>
2021-12-06 11:59:12 -0300

    Atualizando minha associate que nao estava criando o relacionamento

--- models/postcategories.js
+++ models/postcategories.js
@@ -1,22 +1,21 @@
 module.exports = (sequelize, _DataTypes) => {
-    const postsCategorie = sequelize.define('PostsCategories', 
-    {},
-    { timestamps: false });
+  const postsCategorie = sequelize.define('PostsCategories', 
+  {},
+  { timestamps: false });
+
+  // belongsToMany -> cria um relacionamento n:n, usando PostsCategories como tabela de associação
+  postsCategorie.associate = (models) => {
+    models.BlogPosts.belongsToMany(models.Categories, {
+      as: 'categories', // alias da minha associação
+      through: 'PostsCategories', // tabela de associação.
+      foreignKey: 'postId', // referencia a model que chamo a belongsToMany
+      otherKey: 'categoryId' }); // refere ao model com o qual estou criando a associação
   
-    // belongsToMany -> cria um relacionamento n:n, usando PostsCategories como tabela de associação
-    postsCategorie.associate = (models) => {
-      models.BlogPosts.belongsToMany(models.Categories, {
-        as: 'categories', // alias da minha associação
-        through: 'PostsCategories', // tabela de associação.
-        foreignKey: 'postId', // referencia a model que chamo a belongsToMany
-        otherKey: 'categoryId' }); // refere ao model com o qual estou criando a associação
+    models.Categories.belongsToMany(models.BlogPosts, {
+      as: 'posts',
+      through: 'PostsCategories',
+      foreignKey: 'categoryId',
+      otherKey: 'postId' });
     };
-    postsCategorie.associate = (models) => {
-      models.Categories.belongsToMany(models.BlogPosts, {
-        as: 'posts',
-        through: 'PostsCategories',
-        foreignKey: 'categoryId',
-        otherKey: 'postId' });
-      };
-    return postsCategorie;
-  };
+  return postsCategorie;
+};
