f9ae8a5b66072278e1814e362fd2f8b56354bc34
CamilaCode7 <camilafabiabh9@gmail.com>
2021-12-06 14:56:59 -0300

    adicionando return

--- auth/validateToken.js
+++ auth/validateToken.js
@@ -12,7 +12,7 @@ module.exports = async (req, res, next) => {
   const token = req.headers.authorization;
 
   if (!token) {
-    res.status(401).json({ message: 'Token not found' });
+    return res.status(401).json({ message: 'Token not found' });
   }
   try {
     const decoded = jwt.verify(token, secret);
