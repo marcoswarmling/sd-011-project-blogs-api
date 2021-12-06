9a73b9c75bc271c9f7cb654b3e3ab72612aba2f1
Michel Pereira <michelbecauro@gmail.com>
2021-12-06 07:59:44 -0300

    REQ_10: Implements CONTROLLER: put /post/:id

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { post } = require('../services');
 const tokenValidMiddle = require('../validators/middlewares/tokenValidMiddle');
 const postValidMiddle = require('../validators/middlewares/postValidMiddle');
+const postUpdateValidMiddle = require('../validators/middlewares/postUpdateValidMiddle');
 
 const STATUS_CREATED = 201;
 const STATUS_OK = 200;
@@ -9,6 +10,10 @@ const STATUS_OK = 200;
 const createPostsCategories = async (createdPost, categories) => {
   createdPost.addCategories(categories);
 };
+
+// const updatePostsCategories = async (updatedPost, categories) => {
+//   updatedPost.setCategories(categories);
+// };
 
 router.get('/', tokenValidMiddle, async (_req, res, next) => {
   try {
@@ -38,6 +43,20 @@ router.get('/:id', tokenValidMiddle, async (req, res, next) => {
   try {
     const result = await post.getById(id);
     if (result.message) return next(result);
+    res.status(STATUS_OK).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put('/:id', tokenValidMiddle, postUpdateValidMiddle, async (req, res, next) => {
+  const { id: postId } = req.params;
+  // const { body, user: userId, categories } = req;
+  const { body, user: userId } = req;
+  try {
+    const result = await post.updateIt({ postId, body, userId });
+    // updatePostsCategories(result, categories);
+
     res.status(STATUS_OK).json(result);
   } catch (error) {
     next(error);
---
+++ src/validators/middlewares/postUpdateValidMiddle.js
@@ -0,0 +1,78 @@
+const { category: { getAllByArrayIds } } = require('../../services');
+const getByArrayIds = require('../../utils/getByArrayIds');
+
+// Temporary implemetation. It's just a copy of "postVelidMiddle" for now.
+
+const STATUS_BAD_REQUEST = 400;
+const MSG_MISSING_TITLE = '"title" is required';
+const MSG_EMPTY_TITLE = '"title" is not allowed to be empty';
+const MSG_MISSING_CONTENT = '"content" is required';
+const MSG_EMPTY_CONTENT = '"content" is not allowed to be empty';
+const MSG_MISSING_CATEGORY = '"categoryIds" is required';
+const MSG_EMPTY_CATEGORY = '"categoryIds" is not allowed to be empty';
+const MSG_CATEGORY_NOT_FOUND = '"categoryIds" not found';
+
+async function categoryIdsValidator(expressParams) {
+  const { req, res, next } = expressParams;
+  const { body } = req;
+
+  try {
+    if (typeof body.categoryIds === 'undefined') {
+      return res.status(STATUS_BAD_REQUEST).json({ message: MSG_MISSING_CATEGORY });
+    }
+  
+    if (!body.categoryIds) {
+      return res.status(STATUS_BAD_REQUEST).json({ message: MSG_EMPTY_CATEGORY });
+    }
+    const categories = await getByArrayIds(body.categoryIds, getAllByArrayIds);
+  
+    if (body.categoryIds.length !== categories.length) {
+      return res.status(STATUS_BAD_REQUEST).json({ message: MSG_CATEGORY_NOT_FOUND });
+    }
+
+    req.categories = categories;
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+function contentValidator(expressParams) {
+  const { req, res } = expressParams;
+  const { body } = req;
+  const { content } = body;
+
+    if (typeof content === 'undefined') {
+      return res.status(STATUS_BAD_REQUEST).json({ message: MSG_MISSING_CONTENT });
+    }
+  
+    if (!content) {
+      return res.status(STATUS_BAD_REQUEST).json({ message: MSG_EMPTY_CONTENT });
+    }
+  
+    categoryIdsValidator(expressParams);
+}
+
+function titleValidator(expressParams) {
+  const { req, res } = expressParams;
+  const { body } = req;
+  const { title } = body;
+
+    if (typeof title === 'undefined') {
+      return res.status(STATUS_BAD_REQUEST).json({ message: MSG_MISSING_TITLE });
+    }
+  
+    if (!title) {
+      return res.status(STATUS_BAD_REQUEST).json({ message: MSG_EMPTY_TITLE });
+    }
+    
+    contentValidator(expressParams);
+}
+
+module.exports = (req, res, next) => {
+  try {
+    titleValidator({ req, res, next });
+  } catch (error) {
+    next(error);
+  }
+};
