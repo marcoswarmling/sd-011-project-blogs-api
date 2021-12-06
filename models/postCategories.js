68b98880cdddc37ca57d6ae8dcf9388e86468ed9
DiegoCS777 <diegomethal@gmail.com>
2021-12-06 13:58:10 -0300

    lista todos os blogPosts correção

--- models/postCategories.js
+++ models/postCategories.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, _DataTypes) => {
-    const PostCategories = sequelize.define('PostCategories', {}, {});
+    const PostCategories = sequelize.define('PostCategories', {}, { timestamps: false });
     PostCategories.removeAttribute('id');
     return PostCategories;
   };
