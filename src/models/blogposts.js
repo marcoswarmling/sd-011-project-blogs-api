e7c596c139f495ccc7793bff020c66b641229c7a
deltadigo <deltadigo@gmail.com>
2021-12-06 00:42:50 -0300

    Refatoração do model blogposts para função

--- src/models/blogposts.js
+++ src/models/blogposts.js
@@ -1,27 +1,11 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class BlogPosts extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  BlogPosts.init({
+  const BlogPosts = sequelize.define('BlogPosts', {
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: DataTypes.NUMBER,
     published: DataTypes.STRING,
     updated: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'BlogPosts',
   });
+
   return BlogPosts;
 };
