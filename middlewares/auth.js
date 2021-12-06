92c68f17ef4c4320d9da4664120e6a342c920883
gab-boff <gmeninboff@gmail.com>
2021-12-06 01:37:27 -0300

    Req 5 e 6

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,41 +1,135 @@
-const jwt = require('jsonwebtoken');
 const { User } = require('../models');
-require('dotenv').config();
 
-const secret = process.env.JWT_SECRET;
-
-const invalidLoginError = {
+const nameLengthError = {
   status: 400,
-  errorMessage: { message: 'Campos inválidos' },
+  errorMessage: { message: '"displayName" length must be at least 8 characters long' },
 };
 
-const createToken = async (req, res) => {
-  const { email, password } = req.body;
-  const userPayload = await User.findOne({ where: { email, password } });
-  if (!userPayload) {
-    return res.status(invalidLoginError.status).json(invalidLoginError.errorMessage);
-  }
-  const jwtConfig = {
-    expiresIn: '7d',
-    algorithm: 'HS256',
-  };
-  const token = jwt.sign({ userPayload }, secret, jwtConfig);
-  return res.status(200).json({ token });
+const emailFormatError = {
+  status: 400,
+  errorMessage: { message: '"email" must be a valid email' },
 };
 
-const tokenAuth = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization;
-    jwt.verify(token, secret);
-    return next();
-  } catch (_e) {
-    const token = req.headers.authorization;
-    if (!token) return res.status(401).json({ message: 'Token not found' });
-    res.status(401).json({ message: 'Expired or invalid token' });
+const emailRequiredError = {
+  status: 400,
+  errorMessage: { message: '"email" is required' },
+};
+
+const passwordLengthError = {
+  status: 400,
+  errorMessage: { message: '"password" length must be 6 characters long' },
+};
+
+const passwordRequiredError = {
+  status: 400,
+  errorMessage: { message: '"password" is required' },
+};
+
+const repeatedEmailError = {
+  status: 409,
+  errorMessage: { message: 'User already registered' },
+};
+
+const emptyEmailError = {
+  status: 400,
+  errorMessage: { message: '"email" is not allowed to be empty' },
+};
+
+const emptyPasswordError = {
+  status: 400,
+  errorMessage: { message: '"password" is not allowed to be empty' },
+};
+
+const unregisteredEmailError = {
+  status: 400,
+  errorMessage: { message: 'Invalid fields' },
+};
+
+const nameRequiredError = {
+  status: 400,
+  errorMessage: { message: '"name" is required' },
+};
+
+const nameLengthValidation = (req, res, next) => {
+  const { displayName } = req.body;
+  if (displayName.length < 8) {
+    return res.status(nameLengthError.status).json(nameLengthError.errorMessage);
   }
+  next();
+};
+
+const emailValidation = async (req, res, next) => {
+  const { email } = req.body;
+  const regex = /\S+@\S+\.\S+/;
+  if (email === undefined) {
+    return res.status(emailRequiredError.status).json(emailRequiredError.errorMessage);
+  }
+  if (email === '') {
+    return res.status(emptyEmailError.status).json(emptyEmailError.errorMessage);
+  }
+  if (regex.test(email) === false) {
+    return res.status(emailFormatError.status).json(emailFormatError.errorMessage);
+  }
+  const sameEmail = await User.findOne({ where: { email } });
+  if (sameEmail) {
+    return res.status(repeatedEmailError.status).json(repeatedEmailError.errorMessage);
+  }
+  next();
+};
+
+const passwordValidation = (req, res, next) => {
+  const { password } = req.body;
+  if (password === undefined) {
+    return res.status(passwordRequiredError.status).json(passwordRequiredError.errorMessage);
+  }
+  if (password === '') {
+    return res.status(emptyPasswordError.status).json(emptyPasswordError.errorMessage);
+  }
+  if (password.length < 6) {
+    return res.status(passwordLengthError.status).json(passwordLengthError.errorMessage);
+  }
+  next();
+};
+
+const emailLoginValidation = async (req, res, next) => {
+  const { email } = req.body;
+  if (email === undefined) {
+    return res.status(emailRequiredError.status).json(emailRequiredError.errorMessage);
+  }
+  if (email === '') {
+    return res.status(emptyEmailError.status).json(emptyEmailError.errorMessage);
+  }
+  const sameEmail = await User.findOne({ where: { email } });
+  if (!sameEmail) {
+    return res.status(unregisteredEmailError.status).json(unregisteredEmailError.errorMessage);
+  }
+  next();
+};
+
+const passwordLoginValidation = async (req, res, next) => {
+  const { password } = req.body;
+  if (password === undefined) {
+    return res.status(passwordRequiredError.status).json(passwordRequiredError.errorMessage);
+  }
+  if (password === '') {
+    return res.status(emptyPasswordError.status).json(emptyPasswordError.errorMessage);
+  }
+  next();
+};
+
+const categoryValidation = async (req, res, next) => {
+  const { name } = req.body;
+  if (name === undefined) {
+    return res.status(nameRequiredError.status).json(nameRequiredError.errorMessage);
+  }
+  next();
 };
 
 module.exports = {
-  createToken,
-  tokenAuth,
+  nameLengthValidation,
+  emailValidation,
+  passwordValidation,
+  emailLoginValidation,
+  passwordLoginValidation,
+  categoryValidation,
 };
---
+++ routers/categoriesRouter.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const categoriesController = require('../controllers/categoriesController');
+const middlewares = require('../middlewares/userValidations');
+const auth = require('../middlewares/auth');
+
+const router = Router();
+
+router.post('/', auth.tokenAuth,
+  middlewares.categoryValidation,
+  categoriesController.create);
+
+router.get('/', auth.tokenAuth,
+  categoriesController.getAll);
+
+module.exports = router;
